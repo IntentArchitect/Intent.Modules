0e3e8fa0feeca9338ce0ac22f1b21b704b546cd6
jonathanlydall <jonathanl@inoxico.com>
2017-09-18 15:01:24 +0200

    I somehow missed this when pulling across everything else.

---
+++ Modules/Intent.Modules.AspNet.SignalR.Interop.WebApi/Decorator/SignalrWebApiDistributionDecoratorRegistation.cs
@@ -0,0 +1,14 @@
+﻿using Intent.Modules.AspNet.WebApi.Legacy.Controller;
+using Intent.SoftwareFactory.Registrations;
+
+namespace Intent.Modules.AspNet.SignalR.Interop.WebApi.Decorator
+{
+    public class SignalrWebApiDistributionDecoratorRegistation : DecoratorRegistration<IDistributionDecorator>
+    {
+        public override string DecoratorId => SignalrWebApiDistributionDecorator.Identifier;
+        public override object CreateDecoratorInstance()
+        {
+            return new SignalrWebApiDistributionDecorator();
+        }
+    }
+}
