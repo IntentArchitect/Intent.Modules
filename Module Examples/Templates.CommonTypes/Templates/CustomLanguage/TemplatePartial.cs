9a982d4a8d360fb74ae3566a359e729372e42de3
Joel Turner <jturner@getqerent.com>
2018-03-26 21:19:02 +0200

    Tues Work

---
+++ Module Examples/Templates.CommonTypes/Templates/CustomLanguage/TemplatePartial.cs
@@ -0,0 +1,34 @@
+﻿using Intent.MetaModel.Domain;
+using Intent.SoftwareFactory.Engine;
+using Intent.SoftwareFactory.Templates;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Templates.CommonTypes.Templates.CustomLanguage
+{
+    partial class Template : Intent.SoftwareFactory.Templates.IntentProjectItemTemplateBase<IClass>
+    {
+        public const string Identifier = "Templates.CommonTypes.CustomLanguage";
+
+        public Template(IProject project, IClass model)
+            : base(Identifier, project, model)
+        {
+        }
+
+        public override DefaultFileMetaData DefineDefaultFileMetaData()
+        {
+            var result = new DefaultFileMetaData(
+                overwriteBehaviour: OverwriteBehaviour.Always,
+                codeGenType: CodeGenType.Basic,
+                fileName: Model.Name,
+                fileExtension: "cs",
+                defaultLocationInProject: "Templates\\CommonTypes\\CustomLanguage"
+                );
+
+            return result;
+        }
+    }
+}
