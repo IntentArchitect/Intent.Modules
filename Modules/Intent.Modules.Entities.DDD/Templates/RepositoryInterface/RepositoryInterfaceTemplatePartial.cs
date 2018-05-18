7e6ad483800f7b52c0125ede92f52c5353463f4d
garethbaars <garethb@inoxico.com>
2018-05-18 20:29:20 +0200

    Entities.DDD - Repository Interfaces

---
+++ Modules/Intent.Modules.Entities.DDD/Templates/RepositoryInterface/RepositoryInterfaceTemplatePartial.cs
@@ -0,0 +1,36 @@
+﻿using Intent.MetaModel.Domain;
+using Intent.SoftwareFactory.Engine;
+using Intent.SoftwareFactory.Templates;
+
+namespace Intent.Modules.Entities.DDD.Templates.RepositoryInterface
+{
+    partial class RepositoryInterfaceTemplate : IntentRoslynProjectItemTemplateBase<IClass>, ITemplate//, IHasDecorators<AbstractDomainEntityDecorator>
+    {
+        public const string Identifier = "Intent.Entities.DDD.RepositoryInterface";
+
+        public RepositoryInterfaceTemplate(IClass model, IProject project)
+            : base(Identifier, project, model)
+        {
+        }
+
+        public string EntityStateName => Model.Name;
+        public string EntityInterfaceName => $"I{Model.Name}";
+
+        public override RoslynMergeConfig ConfigureRoslynMerger()
+        {
+            return new RoslynMergeConfig(new TemplateMetaData(Id, new TemplateVersion(1, 0)));
+        }
+
+        protected override RoslynDefaultFileMetaData DefineRoslynDefaultFileMetaData()
+        {
+            return new RoslynDefaultFileMetaData(
+                overwriteBehaviour: OverwriteBehaviour.Always,
+                fileName: "I${Model.Name}Repository",
+                fileExtension: "cs",
+                defaultLocationInProject: "Repositories",
+                className: "I${Model.Name}Repository",
+                @namespace: "${Project.ProjectName}"
+                );
+        }
+    }
+}
---
+++ Modules/Intent.Modules.Entities.DDD/Templates/RepositoryInterface/RepositoryInterfaceTemplateRegistration.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using Intent.MetaModel.Domain;
+using Intent.SoftwareFactory;
+using Intent.SoftwareFactory.Engine;
+using Intent.SoftwareFactory.Templates;
+using Intent.SoftwareFactory.Templates.Registrations;
+
+namespace Intent.Modules.Entities.DDD.Templates.RepositoryInterface
+{
+    [Description(RepositoryInterface.RepositoryInterfaceTemplate.Identifier)]
+    public class RepositoryInterfaceTemplateRegistration : ModelTemplateRegistrationBase<IClass>
+    {
+        private readonly IMetaDataManager _metaDataManager;
+
+        public RepositoryInterfaceTemplateRegistration(IMetaDataManager metaDataManager)
+        {
+            _metaDataManager = metaDataManager;
+        }
+
+        public override string TemplateId => RepositoryInterface.RepositoryInterfaceTemplate.Identifier;
+
+        public override ITemplate CreateTemplateInstance(IProject project, IClass model)
+        {
+            return new RepositoryInterface.RepositoryInterfaceTemplate(model, project);
+        }
+
+        public override IEnumerable<IClass> GetModels(Intent.SoftwareFactory.Engine.IApplication application)
+        {
+            return _metaDataManager.GetDomainModels(application);
+        }
+    }
+}
