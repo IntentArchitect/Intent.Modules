1b685916ec81c4f3bc98d0d542641d554bd56aae
dandrejvv <dandrejvv@intentarchitect.com>
2018-08-08 17:50:18 +0200

    DataContract has its own stereotype now and will apply Namespaces and IsReference attributes

--- Modules/Intent.Modules.Application.Contracts/Decorators/DataContractDTOAttributeDecorator.cs
+++ Modules/Intent.Modules.Application.Contracts/Decorators/DataContractDTOAttributeDecorator.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using Intent.MetaModel.DTO;
 using Intent.Modules.Application.Contracts.Templates.DTO;
 using Intent.SoftwareFactory.Templates;
+using Intent.MetaModel.Common;
+using Intent.SoftwareFactory.MetaData;
+using System.Text;
 
 namespace Intent.Modules.Application.Contracts.Decorators
 {
@@ -12,7 +15,8 @@ namespace Intent.Modules.Application.Contracts.Decorators
 
         public string ClasssAttributes(IDTOModel dto)
         {
-            return "[DataContract]";
+            
+            return $"[DataContract{ GetDataContractPropertiesFormatted(dto) }]";
         }
 
         public string PropertyAttributes(IDTOModel dto, IDTOField field)
@@ -23,6 +27,59 @@ namespace Intent.Modules.Application.Contracts.Decorators
         public IEnumerable<string> DeclareUsings()
         {
             yield return "System.Runtime.Serialization";
+        }
+
+        private string GetDataContractPropertiesFormatted(IDTOModel dto)
+        {
+            var dataContractStereotype = GetDataContractStereotype(dto);
+            if (dataContractStereotype != null)
+            {
+                var sb = new StringBuilder();
+
+                var @namespace = dataContractStereotype.GetProperty<string>("Namespace");
+                if (!string.IsNullOrEmpty(@namespace))
+                {
+                    sb.Append($@"Namespace=""{ @namespace }""");
+                }
+
+                var isReference = dataContractStereotype.GetProperty<bool>("IsReference");
+                if(isReference)
+                {
+                    if(sb.Length > 0)
+                    {
+                        sb.Append(" ");
+                    }
+
+                    sb.Append($@"IsReference=""{ isReference }""");
+                }
+
+                if (sb.Length > 0)
+                {
+                    sb.Insert(0, "( ");
+                    sb.Append(" )");
+
+                    return sb.ToString();
+                }
+            }
+            return string.Empty;
+        }
+
+        private IStereotype GetDataContractStereotype(IDTOModel dto)
+        {
+            IStereotype stereotype;
+            stereotype = dto.GetStereotype("DataContract");
+            if(stereotype != null)
+            {
+                return stereotype;
+            }
+
+            stereotype = dto.GetStereotypeInFolders("DataContract");
+            if (stereotype != null)
+            {
+                return stereotype;
+            }
+
+            return null;
         }
     }
 }
