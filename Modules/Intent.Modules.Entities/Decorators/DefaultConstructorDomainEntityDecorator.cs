fa09275ae6282aaa3937bc82c7407bb54184b0e6
dandrejvv <dandrejvv@gmail.com>
2018-09-14 16:29:30 +0200

    Added decorator for adding a default constructor to the Entity classes (ORM purposes)

---
+++ Modules/Intent.Modules.Entities/Decorators/DefaultConstructorDomainEntityDecorator.cs
@@ -0,0 +1,18 @@
+﻿using Intent.MetaModel.Domain;
+using Intent.Modules.Entities.Templates.DomainEntity;
+using System;
+
+namespace Intent.Modules.Entities.Decorators
+{
+    public class DefaultConstructorDomainEntityDecorator : DomainEntityDecoratorBase
+    {
+        public const string Identifier = "Intent.Entities.DefaultConstructorDomainEntityDecorator";
+
+        public override string Constructors(IClass @class)
+        {
+            return $@"        public {@class.Name}()
+        {{
+        }}";
+        }
+    }
+}
---
+++ Modules/Intent.Modules.Entities/Decorators/DefaultConstructorDomainEntityDecoratorRegistration.cs
@@ -0,0 +1,17 @@
+﻿using Intent.Modules.Entities.Templates.DomainEntity;
+using Intent.SoftwareFactory.Engine;
+using Intent.SoftwareFactory.Registrations;
+using System;
+
+namespace Intent.Modules.Entities.Decorators
+{
+    public class DefaultConstructorDomainEntityDecoratorRegistration : DecoratorRegistration<DomainEntityDecoratorBase>
+    {
+        public override string DecoratorId => DefaultConstructorDomainEntityDecorator.Identifier;
+
+        public override object CreateDecoratorInstance(IApplication application)
+        {
+            return new DefaultConstructorDomainEntityDecorator();
+        }
+    }
+}
