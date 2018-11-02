fd3e28e29b4c889d892d555b10b84c0a27354190
garethbaars <gareth.baars@intentarchitect.com>
2018-11-02 09:23:02 +0200

    Intent.Common backward compatibility between 1.5.x and 1.6.x

--- Modules/Intent.Modules.Common/MetaDataExtensions.cs
+++ Modules/Intent.Modules.Common/MetaDataExtensions.cs
@@ -14,22 +14,46 @@ namespace Intent.SoftwareFactory
     {
         public static IEnumerable<IServiceModel> GetServiceModels(this Engine.IMetaDataManager metaDataManager, Engine.IApplication application, string metadataIdentifier = "Services")
         {
-            return metaDataManager.GetMetaData<IServiceModel>(new MetaDataIdentifier(metadataIdentifier ?? "Services")).Where(x => x.Application.Name == application.ApplicationName).ToList();
+            var result = metaDataManager.GetMetaData<IServiceModel>(new MetaDataIdentifier(metadataIdentifier ?? "Services")).Where(x => x.Application.Name == application.ApplicationName).ToList();
+            if (result.Any())
+            {
+                return result;
+            }
+            // Purely for backward compatibility between 1.5.x and 1.6.x
+            return metaDataManager.GetMetaData<IServiceModel>(new MetaDataIdentifier("Service")).Where(x => x.Application.Name == application.ApplicationName).ToList();
         }
 
         public static IEnumerable<IClass> GetDomainModels(this Engine.IMetaDataManager metaDataManager, Engine.IApplication application, string metadataIdentifier = "Domain")
         {
-            return metaDataManager.GetMetaData<IClass>(new MetaDataIdentifier(metadataIdentifier ?? "Domain")).Where(x => x.Application.Name == application.ApplicationName).ToList();
+            var result = metaDataManager.GetMetaData<IClass>(new MetaDataIdentifier(metadataIdentifier ?? "Domain")).Where(x => x.Application.Name == application.ApplicationName).ToList();
+            if (result.Any())
+            {
+                return result;
+            }
+            // Purely for backward compatibility between 1.5.x and 1.6.x
+            return metaDataManager.GetMetaData<IClass>(new MetaDataIdentifier("DomainEntity")).Where(x => x.Application.Name == application.ApplicationName).ToList();
         }
         
         public static IEnumerable<IDTOModel> GetDTOModels(this Engine.IMetaDataManager metaDataManager, Engine.IApplication application, string metadataIdentifier = "Services")
         {
-            return metaDataManager.GetMetaData<IDTOModel>(new MetaDataIdentifier(metadataIdentifier ?? "Services")).Where(x => x.Application.Name == application.ApplicationName).ToList();
+            var result = metaDataManager.GetMetaData<IDTOModel>(new MetaDataIdentifier(metadataIdentifier ?? "Services")).Where(x => x.Application.Name == application.ApplicationName).ToList();
+            if (result.Any())
+            {
+                return result;
+            }
+            // Purely for backward compatibility between 1.5.x and 1.6.x
+            return metaDataManager.GetMetaData<IDTOModel>(new MetaDataIdentifier("DTO")).Where(x => x.Application.Name == application.ApplicationName).ToList();
         }
         
         public static IEnumerable<IEnumModel> GetEnumModels(this Engine.IMetaDataManager metaDataManager, Engine.IApplication application, string metadataIdentifier)
         {
-            return metaDataManager.GetMetaData<IEnumModel>(new MetaDataIdentifier(metadataIdentifier)).Where(x => x.Application.Name == application.ApplicationName).ToList();
+            var result = metaDataManager.GetMetaData<IEnumModel>(new MetaDataIdentifier(metadataIdentifier)).Where(x => x.Application.Name == application.ApplicationName).ToList();
+            if (result.Any())
+            {
+                return result;
+            }
+            // Purely for backward compatibility between 1.5.x and 1.6.x
+            return metaDataManager.GetMetaData<IEnumModel>(new MetaDataIdentifier("Enum")).Where(x => x.Application.Name == application.ApplicationName).ToList();
         }
     }
 }
