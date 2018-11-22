dbe80023a19a277151b329ee6b9c5c2911a90805
garethbaars <gareth.baars@intentarchitect.com>
2018-11-22 12:25:45 +0200

    prevent template dependencies from adding using clauses in Startup.cs for AspNetCore

--- Modules/Intent.Modules.AspNetCore/Templates/Startup/CoreWebStartupTemplatePartial.cs
+++ Modules/Intent.Modules.AspNetCore/Templates/Startup/CoreWebStartupTemplatePartial.cs
@@ -199,6 +199,8 @@ namespace Intent.Modules.AspNetCore.Templates.Startup
                 );
         }
 
+        public override string DependencyUsings => "";
+
         public IEnumerable<ITemplateDependancy> GetTemplateDependencies()
         {
             return _registrations
