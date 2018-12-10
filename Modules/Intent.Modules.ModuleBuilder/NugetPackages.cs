62ae9b5c302f246534010712862efac54091d8be
garethbaars <gareth.baars@intentarchitect.com>
2018-12-10 13:31:56 +0200

    icons for metadata packages

--- Modules/Intent.Modules.ModuleBuilder/NugetPackages.cs
+++ Modules/Intent.Modules.ModuleBuilder/NugetPackages.cs
@@ -9,7 +9,7 @@ namespace Intent.Modules.ModuleBuilder
 {
     public static class NugetPackages
     {
-        public static NugetPackageInfo IntentArchitectPackager = new NugetPackageInfo("Intent.IntentArchitectPackager", "1.2.2");
+        public static NugetPackageInfo IntentArchitectPackager = new NugetPackageInfo("Intent.IntentArchitectPackager", "1.3.0");
         public static NugetPackageInfo IntentMetadata = new NugetPackageInfo("Intent.SoftwareFactory.MetaModel", "1.7.0-pre4");
         public static NugetPackageInfo IntentModulesCommon = new NugetPackageInfo("Intent.Modules.Common", "1.7.0-pre4");
         public static NugetPackageInfo IntentRoslynWeaverAttributes = new NugetPackageInfo("Intent.RoslynWeaver.Attributes", "1.0.0");
