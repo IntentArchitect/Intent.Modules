b079f7bb56686ed384f82bb424f7f11e1cc94448
dandrejvv <dandrejvv@gmail.com>
2019-01-14 10:10:23 +0200

    Basic Identity server 4 setup. Will test then extend with more practical config and potential AspID integration

---
+++ Modules/Intent.Modules.IdentityServer4/NugetPackages.cs
@@ -0,0 +1,10 @@
+﻿using Intent.Modules.Common.VisualStudio;
+using System;
+
+namespace Intent.Modules.IdentityServer4
+{
+    public static class NugetPackages
+    {
+        public static readonly NugetPackageInfo IdentityServer4 = new NugetPackageInfo("IdentityServer4", "2.3.2");
+    }
+}
