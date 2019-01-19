b18cc42bbed683eff8a13935b08dcfd3d9d9ceb8
dandrejvv <dandrejvv@gmail.com>
2019-01-19 11:00:45 +0200

    Hooked up the new IdentityServer4 module to the Intent managed Module Builder. This will be used to complete that module and to manage the newly developed convention modules

--- Modules/Intent.Modules.IdentityServer4/Templates/IdentityConfig/IdentityConfigPartial.cs
+++ Modules/Intent.Modules.IdentityServer4/Templates/IdentityConfig/IdentityConfigPartial.cs
@@ -5,9 +5,15 @@ using Intent.Metadata.Models;
 using Intent.Modules.Common.Templates;
 using Intent.SoftwareFactory.Engine;
 using Intent.SoftwareFactory.Templates;
+using Intent.Modules.Common;
+using Intent.RoslynWeaver.Attributes;
+
+[assembly: IntentTemplate("Intent.ModuleBuilder.RoslynProjectItemTemplate.Partial", Version = "1.0")]
+[assembly: DefaultIntentManaged(Mode.Merge)]
 
 namespace Intent.Modules.IdentityServer4.Templates.IdentityConfig
 {
+    [IntentManaged(Mode.Merge, Body = Mode.Merge, Signature = Mode.Fully)]
     partial class IdentityConfig : IntentRoslynProjectItemTemplateBase<object>
     {
         public const string TemplateId = "IdentityServer4.IdentityConfig";
@@ -21,6 +27,7 @@ namespace Intent.Modules.IdentityServer4.Templates.IdentityConfig
             return new RoslynMergeConfig(new TemplateMetaData(Id, "1.0"));
         }
 
+        [IntentManaged(Mode.Merge, Body = Mode.Ignore, Signature = Mode.Fully)]
         protected override RoslynDefaultFileMetaData DefineRoslynDefaultFileMetaData()
         {
             return new RoslynDefaultFileMetaData(
--- Modules/Intent.Modules.IdentityServer4/Templates/IdentityConfig/IdentityConfigRegistration.cs
+++ Modules/Intent.Modules.IdentityServer4/Templates/IdentityConfig/IdentityConfigRegistration.cs
@@ -6,9 +6,14 @@ using Intent.Modules.Common;
 using Intent.Modules.Common.Registrations;
 using Intent.SoftwareFactory.Engine;
 using Intent.SoftwareFactory.Templates;
+using Intent.RoslynWeaver.Attributes;
+
+[assembly: IntentTemplate("Intent.ModuleBuilder.TemplateRegistration.SingleFileNoModel", Version = "1.0")]
+[assembly: DefaultIntentManaged(Mode.Merge)]
 
 namespace Intent.Modules.IdentityServer4.Templates.IdentityConfig
 {
+    [IntentManaged(Mode.Merge, Body = Mode.Merge, Signature = Mode.Fully)]
     public class IdentityConfigRegistration : NoModelTemplateRegistrationBase
     {
         public override string TemplateId => IdentityConfig.TemplateId;
