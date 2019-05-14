5d1c54b8f8e2ebd3b889d30b783364847e2f4f63
garethbaars <gareth.baars@intentarchitect.com>
2019-05-14 20:13:30 +0200

    experimenting with Angular modeler

--- Modules/Intent.Modules.Common/Registrations/DecoratorRegistration.cs
+++ Modules/Intent.Modules.Common/Registrations/DecoratorRegistration.cs
@@ -25,19 +25,6 @@ namespace Intent.Modules.Common.Registrations
 
         public abstract string DecoratorId { get; }
 
-        //public void DoRegistration(Action<string, Type, object> register, IApplication application)
-        //{
-        //    var config = application.Config.GetConfig(this.DecoratorId, PluginConfigType.Decorator);
-        //    if (!config.Enabled)
-        //    {
-        //        Logging.Log.Info($"Skipping disabled Decorator : { DecoratorId }.");
-        //        return;
-        //    }
-
-        //    register(DecoratorId, typeof(TDecoratorContract), CreateDecoratorInstance(application));
-        //}
-
-        //public abstract object CreateDecoratorInstance(IApplication application);
         public abstract TDecoratorContract CreateDecoratorInstance(TTemplate template, IApplication application);
     }
 
