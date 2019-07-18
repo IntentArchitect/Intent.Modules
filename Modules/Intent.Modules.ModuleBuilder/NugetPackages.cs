0b51835ba36123e2a90a8a80154b1f08d2ca6c40
Jonathan Lydall <jonathan.lydall@intentarchitect.com>
2019-07-18 20:49:02 +0200

    Fix the module builder template and NuGet package dependencies.

--- Modules/Intent.Modules.ModuleBuilder/NugetPackages.cs
+++ Modules/Intent.Modules.ModuleBuilder/NugetPackages.cs
@@ -7,7 +7,7 @@ namespace Intent.Modules.ModuleBuilder
         public static NugetPackageInfo IntentArchitectPackager = new NugetPackageInfo("Intent.IntentArchitectPackager", "1.9.3")
             .SpecifyAssetsBehaviour(new[] { "all" }, new[] { "runtime", "build", "native", "contentfiles", "analyzers", "buildtransitive" });
         public static NugetPackageInfo IntentSdk = new NugetPackageInfo("Intent.SoftwareFactory.SDK", "2.0.0-pre6");
-        public static NugetPackageInfo IntentModulesCommon = new NugetPackageInfo("Intent.Modules.Common", "2.0.0-pre3");
+        public static NugetPackageInfo IntentModulesCommon = new NugetPackageInfo("Intent.Modules.Common", "2.0.0-pre4");
         public static NugetPackageInfo IntentRoslynWeaverAttributes = new NugetPackageInfo("Intent.RoslynWeaver.Attributes", "1.0.0");
     }
 
