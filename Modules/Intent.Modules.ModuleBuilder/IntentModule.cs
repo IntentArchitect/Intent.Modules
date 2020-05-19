c1f5d82ea86a52dc7038ec404e8ce00105c32e50
garethbaars <gareth.baars@intentarchitect.com>
2020-05-19 16:01:35 +0200

    Tweaks to modules for 2.1.0 release

--- Modules/Intent.Modules.ModuleBuilder/IntentModule.cs
+++ Modules/Intent.Modules.ModuleBuilder/IntentModule.cs
@@ -7,7 +7,7 @@ namespace Intent.Modules.ModuleBuilder
     public class IntentModule
     {
         public static readonly IntentModule IntentRoslynWeaver = new IntentModule("Intent.OutputManager.RoslynWeaver", "2.0.0");
-        public static readonly IntentModule IntentCommon = new IntentModule("Intent.Common", "2.1.0");
+        public static readonly IntentModule IntentCommon = new IntentModule("Intent.Common", "2.0.4");
         public static readonly IntentModule IntentCommonTypes = new IntentModule("Intent.Common.Types", "2.0.1");
 
         public IntentModule(string name, string version)
