e491063f2de93249ea644181c6bbdb52b79e3391
garethbaars <gareth.baars@intentarchitect.com>
2020-08-22 20:36:32 +0200

    Missing code from previous commit

---
+++ Modules/Intent.Modules.VisualStudio.Projects/Templates/IVisualStudioProjectTemplate.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Intent.Engine;
+using Intent.Modules.Common.VisualStudio;
+using Intent.Modules.VisualStudio.Projects.Api;
+
+namespace Intent.Modules.VisualStudio.Projects.Templates
+{
+    public interface IVisualStudioProjectTemplate
+    {
+        string ProjectId { get; }
+        string Name { get; }
+        string FilePath { get; }
+        string LoadContent();
+        IEnumerable<INugetPackageInfo> RequestedNugetPackages();
+    }
+}
