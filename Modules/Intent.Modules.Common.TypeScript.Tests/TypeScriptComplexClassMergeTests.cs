92ce7a4a2cb6150957df9c1f7f0671012d5431f7
garethbaars <gareth.baars@intentarchitect.com>
2020-09-07 15:03:18 +0200

    missing tests

---
+++ Modules/Intent.Modules.Common.TypeScript.Tests/TypeScriptComplexClassMergeTests.cs
@@ -0,0 +1,114 @@
+﻿using Intent.Modules.Common.TypeScript.Weaving;
+using Xunit;
+
+namespace Intent.Modules.Common.TypeScript.Tests
+{
+    public class TypeScriptComplexClassMergeTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            var merger = new TypeScriptWeavingMerger();
+            var result = merger.Merge(existingContent: @"import { Component, NgModule } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styles: []
+})
+@NewDecorator()
+export class AppComponent {
+  title = 'AngularApp';
+
+  constructor() {
+      this.title = ""Other"";
+    }
+
+
+    @IntentIgnore()
+    customMethod() {
+    }
+}", outputContent: @"import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styles: []
+})
+export class AppComponent {
+  title = 'AngularApp';
+
+  addAMethod() {
+  }
+}");
+            Assert.Equal(@"import { Component, NgModule } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styles: []
+})
+export class AppComponent {
+  title = 'AngularApp';
+
+  addAMethod() {
+  }
+
+
+    @IntentIgnore()
+    customMethod() {
+    }
+}", result);
+            ;
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var merger = new TypeScriptWeavingMerger();
+            var result = merger.Merge(existingContent: @"import { Component, NgModule } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styles: []
+})
+export class AppComponent {
+  title = 'AngularApp';
+
+  constructor() {
+      this.title = ""Other"";
+    }
+
+
+    customMethod() {
+    }
+}", outputContent: @"import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styles: []
+})
+export class AppComponent {
+  title = 'AngularApp';
+
+  addAMethod() {
+  }
+}");
+            Assert.Equal(@"import { Component, NgModule } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styles: []
+})
+export class AppComponent {
+  title = 'AngularApp';
+
+  addAMethod() {
+  }
+}", result);
+        }
+    }
+}
