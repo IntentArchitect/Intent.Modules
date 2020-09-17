c88fbce1a4d469f098c0a6ca1b10529d067ea0b6
garethbaars <gareth.baars@intentarchitect.com>
2020-09-17 17:35:36 +0200

    Angular: Intent Decorators template

---
+++ Modules/Intent.Modules.Angular/Templates/Shared/IntentDecoratorsTemplate/IntentDecoratorsTemplateRegistration.cs
@@ -0,0 +1,26 @@
+using Intent.Engine;
+using Intent.Modules.Common.Registrations;
+using Intent.RoslynWeaver.Attributes;
+using Intent.Templates;
+using Intent.Metadata.Models;
+using Intent.Modules.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+[assembly: DefaultIntentManaged(Mode.Merge)]
+[assembly: IntentTemplate("Intent.ModuleBuilder.TemplateRegistration.SingleFileNoModel", Version = "1.0")]
+
+namespace Intent.Modules.Angular.Templates.Shared.IntentDecoratorsTemplate
+{
+    [IntentManaged(Mode.Merge, Body = Mode.Merge, Signature = Mode.Fully)]
+    public class IntentDecoratorsTemplateRegistration : NoModelTemplateRegistrationBase
+    {
+        public override string TemplateId => IntentDecoratorsTemplate.TemplateId;
+
+        public override ITemplate CreateTemplateInstance(IProject project)
+        {
+            return new IntentDecoratorsTemplate(project, null);
+        }
+    }
+}
