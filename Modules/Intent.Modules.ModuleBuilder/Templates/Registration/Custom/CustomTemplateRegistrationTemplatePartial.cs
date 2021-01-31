2970b9899a678325225714aeef053a99956a4eb2
Jonathan Lydall <jonathan.lydall@intentarchitect.com>
2021-01-31 21:59:06 +0200

    Ensure suffixed with `Template`.

--- Modules/Intent.Modules.ModuleBuilder/Templates/Registration/Custom/CustomTemplateRegistrationTemplatePartial.cs
+++ Modules/Intent.Modules.ModuleBuilder/Templates/Registration/Custom/CustomTemplateRegistrationTemplatePartial.cs
@@ -1,16 +1,11 @@
-﻿using Intent.Metadata.Models;
-using Intent.Modules.Common;
-using Intent.Modules.Common.Templates;
-using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using Intent.Engine;
-using Intent.Modules.Common.CSharp;
+using Intent.ModuleBuilder.Api;
 using Intent.Modules.Common.CSharp.Templates;
+using Intent.Modules.Common.Templates;
 using Intent.Modules.Common.Types.Api;
 using Intent.Modules.Common.VisualStudio;
-using Intent.ModuleBuilder.Api;
-using Intent.Templates;
 
 namespace Intent.Modules.ModuleBuilder.Templates.Registration.Custom
 {
@@ -38,21 +33,9 @@ namespace Intent.Modules.ModuleBuilder.Templates.Registration.Custom
                 relativeLocation: $"{FolderPath}");
         }
 
-        //protected override RoslynDefaultFileMetadata DefineRoslynDefaultFileMetadata()
-        //{
-        //    return new RoslynDefaultFileMetadata(
-        //        overwriteBehaviour: OverwriteBehaviour.Always,
-        //        fileName: "${Model.Name}Registration",
-        //        fileExtension: "cs",
-        //        relativeLocation: "${FolderPath}/${Model.Name}",
-        //        className: "${Model.Name}Registration",
-        //        @namespace: "${Project.Name}.${FolderNamespace}.${Model.Name}"
-        //    );
-        //}
-
         private string GetTemplateNameForTemplateId()
         {
-            return Model.Name.Replace("Registrations", "Template");
+            return $"{Model.Name.Replace("Registrations", "Template").RemoveSuffix("Template")}Template";
         }
     }
 }
