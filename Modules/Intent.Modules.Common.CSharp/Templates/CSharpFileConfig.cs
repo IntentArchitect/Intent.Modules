904abf71bfb6e83c0039b8842e8933238e5cc65f
dandrejvv <dandrejvv@gmail.com>
2021-03-19 19:34:12 +0200

    We don't want to complicate the users' experience by having multiple constructors just to have backward compatibility and satisfying the constructor. Since a lot of code relies on the current constructor let's extend with more optional settings using properties instead

--- Modules/Intent.Modules.Common.CSharp/Templates/CSharpFileConfig.cs
+++ Modules/Intent.Modules.Common.CSharp/Templates/CSharpFileConfig.cs
@@ -20,53 +20,38 @@ namespace Intent.Modules.Common.CSharp.Templates
             OverwriteBehaviour overwriteBehaviour = OverwriteBehaviour.Always,
             string fileName = null,
             string fileExtension = "cs",
-            string dependsUpon = null) : this(
-                className: className,
-                @namespace: @namespace,
-                relativeLocation: relativeLocation,
-                overwriteBehaviour: overwriteBehaviour,
-                fileName: fileName,
-                fileExtension: fileExtension,
-                dependsUpon: dependsUpon,
-                autoFormat: true)
-        {
-            // This overload is for backwards compatibility for templates which were compiled
-            // against this method signature. Even though the below overload's new 'autoFormat'
-            // parameter is optional, C# assemblies compiled against it before its introduction
-            // don't see it as the same method and throw a method not found exception when trying
-            // to call it.
-        }
-
-        /// <summary>
-        /// Sets the C# file configuration
-        /// </summary>
-        public CSharpFileConfig(
-            string className,
-            string @namespace,
-            string relativeLocation,
-            OverwriteBehaviour overwriteBehaviour,
-            string fileName,
-            string fileExtension,
-            string dependsUpon,
-            bool autoFormat)
+            string dependsUpon = null)
             : base(fileName ?? className, fileExtension, relativeLocation, overwriteBehaviour, "RoslynWeave")
         {
-            // This overload has to be void of optional parameters because the compiler will not be
-            // able to differentiate between the two constructors if none of the optional parameters
-            // are not specified. So this overload will have to be explicitly set while the other one
-            // offers a more convenient setup.
-
             CustomMetadata["ClassName"] = className ?? throw new ArgumentNullException(nameof(className));
-            CustomMetadata["AutoFormat"] = autoFormat.ToString();
-            
+
             if (!string.IsNullOrWhiteSpace(@namespace))
             {
                 CustomMetadata["Namespace"] = @namespace;
             }
-            
+
             if (!string.IsNullOrWhiteSpace(dependsUpon))
             {
                 CustomMetadata["Depends On"] = dependsUpon;
+            }
+
+            AutoFormat = true;
+        }
+
+        /// <summary>
+        /// C# styling automatically formatted
+        /// </summary>
+        public bool AutoFormat
+        {
+            get
+            {
+                var val = CustomMetadata["AutoFormat"];
+                if (string.IsNullOrEmpty(val)) { return false; }
+                return bool.Parse(val);
+            }
+            set 
+            {
+                CustomMetadata["AutoFormat"] = value.ToString();
             }
         }
     }
