b6a2c9f1bd0fe57f966106e176ae8d2ac59c5a74
Jonathan Lydall <jonathan.lydall@intentarchitect.com>
2021-12-31 17:02:18 +0200

    AB#398 - C# nullable reference types not working for .net6.0 projects

---
+++ Modules/Intent.Modules.Common.CSharp.Tests/VisualStudio/CSharpProjectTests.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using Intent.Engine;
+using Intent.Modules.Common.CSharp.VisualStudio;
+using NSubstitute;
+using Shouldly;
+using Xunit;
+
+namespace Intent.Modules.Common.CSharp.Tests.VisualStudio
+{
+    public class CSharpProjectTests
+    {
+        public class DescribeIsNullableAwareContext
+        {
+            [Theory]
+            [InlineData(true, "net5.0", "default", true)]
+            [InlineData(true, "net6.0", "default", true)]
+            [InlineData(true, "net7.0", "default", true)]
+            [InlineData(true, "netcoreapp1.0", "default", false)]
+            [InlineData(true, "netcoreapp1.1", "default", false)]
+            [InlineData(true, "netcoreapp2.0", "default", false)]
+            [InlineData(true, "netcoreapp2.1", "default", false)]
+            [InlineData(true, "netcoreapp2.2", "default", false)]
+            [InlineData(true, "netcoreapp3.0", "default", true)]
+            [InlineData(true, "netcoreapp3.1", "default", true)]
+            [InlineData(true, "net45", "default", false)]
+            [InlineData(true, "net451", "default", false)]
+            [InlineData(true, "net452", "default", false)]
+            [InlineData(true, "net46", "default", false)]
+            [InlineData(true, "net461", "default", false)]
+            [InlineData(true, "net462", "default", false)]
+            [InlineData(true, "net47", "default", false)]
+            [InlineData(true, "net471", "default", false)]
+            [InlineData(true, "net472", "default", false)]
+            [InlineData(true, "net48", "default", false)]
+            [InlineData(true, "netstandard1.0", "default", false)]
+            [InlineData(true, "netstandard1.1", "default", false)]
+            [InlineData(true, "netstandard1.2", "default", false)]
+            [InlineData(true, "netstandard1.3", "default", false)]
+            [InlineData(true, "netstandard1.4", "default", false)]
+            [InlineData(true, "netstandard1.5", "default", false)]
+            [InlineData(true, "netstandard1.6", "default", false)]
+            [InlineData(true, "netstandard2.0", "default", false)]
+            [InlineData(true, "netstandard2.1", "default", true)]
+            [InlineData(true, null, "latest", true)]
+            [InlineData(true, null, "preview", true)]
+            [InlineData(true, null, "8.1", true)]
+            [InlineData(true, null, "8.0", true)]
+            [InlineData(true, null, "7.9", false)]
+            [InlineData(false, null, "latest", false)]
+            [InlineData(false, null, "preview", false)]
+            [InlineData(false, null, "8.1", false)]
+            [InlineData(false, null, "8.0", false)]
+            [InlineData(false, null, "7.9", false)]
+            [InlineData(false, null, null, false)]
+            public void ItShouldReturnTheCorrectResult(bool nullableEnabled, string targetFramework, string languageVersion, bool expected)
+            {
+                // Arrange
+                var outputTarget = Substitute.For<IOutputTarget>();
+                outputTarget.Metadata.Returns(new Dictionary<string, object>
+                {
+                    ["Nullable Enabled"] = nullableEnabled,
+                    ["Language Version"] = languageVersion,
+                    ["Target Frameworks"] = new[] { targetFramework }
+                });
+                var sut = new CSharpProject(outputTarget);
+
+                // Act
+                var result = sut.IsNullableAwareContext();
+
+                // Assert
+                result.ShouldBe(expected);
+            }
+        }
+    }
+}
--- Modules/Intent.Modules.Common.CSharp/VisualStudio/OutputTargetExtensions.cs
+++ Modules/Intent.Modules.Common.CSharp/VisualStudio/OutputTargetExtensions.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using Intent.Engine;
 using Intent.Templates;
 
@@ -18,7 +17,7 @@ namespace Intent.Modules.Common.CSharp.VisualStudio
         bool IsNullableAwareContext();
     }
 
-    class CSharpProject : ICSharpProject
+    internal class CSharpProject : ICSharpProject
     {
         private readonly IOutputTarget _outputTarget;
 
@@ -55,17 +54,88 @@ namespace Intent.Modules.Common.CSharp.VisualStudio
 
         public bool IsNullableAwareContext()
         {
-            if (LanguageVersion == "latest" || (decimal.TryParse(LanguageVersion, out var langVersion) && langVersion >= 8.0m))
+            if (!NullableEnabled)
             {
-                return NullableEnabled;
+                return false;
             }
 
-            if (LanguageVersion == "default" && TargetFrameworks.All(x => x.StartsWith("netcoreapp3") || x.Equals("netstandard2.1") || x.Equals("net5.0")))
+            switch (LanguageVersion)
             {
-                return NullableEnabled;
+                case "latest":
+                case "preview":
+                    return true;
+                case "default":
+                    return TargetFrameworks.All(DefaultSupportsNullable);
+                default:
+                    return decimal.Parse(LanguageVersion) >= 8.0m;
+            }
+        }
+
+        /// <summary>
+        /// Based on data from the below links, returns true if the default language version for
+        /// the provided <paramref name="frameworkMoniker"/> is at least 8.0.
+        /// <br/>
+        /// https://docs.microsoft.com/dotnet/csharp/language-reference/configure-language-version#defaults
+        /// <br/>
+        /// https://docs.microsoft.com/dotnet/standard/frameworks#supported-target-frameworks
+        /// </summary>
+        private static bool DefaultSupportsNullable(string frameworkMoniker)
+        {
+            switch (frameworkMoniker)
+            {
+                case "net11":
+                case "net20":
+                case "net35":
+                case "net40":
+                case "net403":
+                case "net45":
+                case "net451":
+                case "net452":
+                case "net46":
+                case "net461":
+                case "net462":
+                case "net47":
+                case "net471":
+                case "net472":
+                case "net48":
+                    // Language version 7.3
+                    return false;
+                case "netcoreapp1.0":
+                case "netcoreapp1.1":
+                case "netcoreapp2.0":
+                case "netcoreapp2.1":
+                case "netcoreapp2.2":
+                    // Language version 7.3
+                    return false;
+                case "netcoreapp3.0":
+                case "netcoreapp3.1":
+                    // Language version 8.0
+                    return true;
+                case "netstandard1.0":
+                case "netstandard1.1":
+                case "netstandard1.2":
+                case "netstandard1.3":
+                case "netstandard1.4":
+                case "netstandard1.5":
+                case "netstandard1.6":
+                case "netstandard2.0":
+                    // Language version 7.3
+                    return false;
+                case "netstandard2.1":
+                    // Language version 8.0
+                    return true;
             }
 
-            return false;
+            // Match for strings like net5.0, net6.0, etc.
+            // net5.0 is version 9.0
+            if (frameworkMoniker.StartsWith("net") &&
+                frameworkMoniker.Contains(".") &&
+                float.TryParse(frameworkMoniker.Substring(3), out _))
+            {
+                return true;
+            }
+
+            throw new Exception($"Could not determine default language version for framework moniker '{frameworkMoniker}'");
         }
 
         public bool IsNetCore2App => GetSupportedFrameworks().Any(x => x.StartsWith("netcoreapp2"));
