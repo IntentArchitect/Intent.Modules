cf25d8d5ba149d6572582a144c2353b34c677260
Jonathan Lydall <jonathan.lydall@intentarchitect.com>
2021-12-31 17:01:36 +0200

    AB#398 - Post clean-up, move types into separate files (no other changes)

---
+++ Modules/Intent.Modules.Common.CSharp/VisualStudio/ICSharpProject.cs
@@ -0,0 +1,15 @@
+﻿using Intent.Engine;
+
+namespace Intent.Modules.Common.CSharp.VisualStudio
+{
+    public interface ICSharpProject : IOutputTarget
+    {
+        string LanguageVersion { get; }
+        bool NullableEnabled { get; }
+        bool IsNetCore2App { get; }
+        bool IsNetCore3App { get; }
+        bool IsNet4App { get; }
+        bool IsNet5App { get; }
+        bool IsNullableAwareContext();
+    }
+}
