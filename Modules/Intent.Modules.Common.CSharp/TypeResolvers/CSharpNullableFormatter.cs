110d9382edee71ca7cc8064469143391ba296762
Jonathan Lydall <jonathan.lydall@intentarchitect.com>
2022-01-28 11:05:07 +0200

    AB#452 - C# nullable reference type issues
    Add XML docs.

--- Modules/Intent.Modules.Common.CSharp/TypeResolvers/CSharpNullableFormatter.cs
+++ Modules/Intent.Modules.Common.CSharp/TypeResolvers/CSharpNullableFormatter.cs
@@ -4,15 +4,22 @@ using Intent.Modules.Common.TypeResolution;
 
 namespace Intent.Modules.Common.CSharp.TypeResolvers
 {
+    /// <summary>
+    /// C# implementation of <see cref="INullableFormatter"/>.
+    /// </summary>
     public class CSharpNullableFormatter : INullableFormatter
     {
         private readonly ICSharpProject _project;
 
+        /// <summary>
+        /// Creates a new instance of <see cref="CSharpNullableFormatter"/>.
+        /// </summary>
         public CSharpNullableFormatter(ICSharpProject project)
         {
             _project = project;
         }
 
+        /// <inheritdoc />
         public string AsNullable(IResolvedTypeInfo typeInfo)
         {
             if (typeInfo.IsNullable && !IsInterface(typeInfo) &&
@@ -25,7 +32,7 @@ namespace Intent.Modules.Common.CSharp.TypeResolvers
             return typeInfo.Name;
         }
 
-        private bool IsInterface(IResolvedTypeInfo typeInfo)
+        private static bool IsInterface(IResolvedTypeInfo typeInfo)
         {
             return (!typeInfo.IsPrimitive && typeInfo.Name.StartsWith("I") && typeInfo.Name.Length >= 2 && char.IsUpper(typeInfo.Name[1]));
         }
