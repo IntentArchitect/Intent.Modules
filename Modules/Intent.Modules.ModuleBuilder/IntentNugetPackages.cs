870bfaeddb8a52a3b06428dae64e3cc08f12db9b
garethbaars <gareth.baars@intentarchitect.com>
2022-01-28 07:23:53 +0200

    update ModuleBuilder nuget references; updated common typescript to latest.

--- Modules/Intent.Modules.ModuleBuilder/IntentNugetPackages.cs
+++ Modules/Intent.Modules.ModuleBuilder/IntentNugetPackages.cs
@@ -6,10 +6,10 @@ namespace Intent.Modules.ModuleBuilder
     {
         public static NugetPackageInfo IntentSdk = new NugetPackageInfo("Intent.SoftwareFactory.SDK", "3.2.0");
 
-        public static NugetPackageInfo IntentPackager = new NugetPackageInfo("Intent.Packager", "3.1.0")
+        public static NugetPackageInfo IntentPackager = new NugetPackageInfo("Intent.Packager", "3.2.0")
             .SpecifyAssetsBehaviour(new[] { "all" }, new[] { "runtime", "build", "native", "contentfiles", "analyzers", "buildtransitive" });
 
         public static NugetPackageInfo IntentModulesCommon = new NugetPackageInfo("Intent.Modules.Common", "3.2.0");
-        public static NugetPackageInfo IntentModulesCommonTypes = new NugetPackageInfo("Intent.Modules.Common.Types", "3.1.1");
+        public static NugetPackageInfo IntentModulesCommonTypes = new NugetPackageInfo("Intent.Modules.Common.Types", "3.2.0");
     }
 }
