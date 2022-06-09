a7f98f93bb1dd291f03e60a427353ef3124fa1a1
Jonathan Lydall <jonathan.lydall@intentarchitect.com>
2022-06-09 18:12:45 +0200

    AB#731 - Update GetTypeName so it adds using directives for Type Definitions
    SQL done.

--- Modules/Intent.Modules.Common.Sql/SqlTypeResolver.cs
+++ Modules/Intent.Modules.Common.Sql/SqlTypeResolver.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using Intent.Metadata.Models;
 using Intent.Modules.Common.TypeResolution;
 
@@ -16,88 +14,94 @@ namespace Intent.Modules.Common.Sql
         {
             return new SqlTypeResolverContext();
         }
-    }
 
-    public class SqlTypeResolverContext : TypeResolverContextBase
-    {
-        public SqlTypeResolverContext() : base(new CollectionFormatter("{0}"), new DefaultNullableFormatter())
+        public class SqlTypeResolverContext : TypeResolverContextBase
         {
-        }
-
-        protected override string FormatGenerics(IResolvedTypeInfo type, IEnumerable<IResolvedTypeInfo> genericTypes)
-        {
-            throw new NotImplementedException();
-        }
-
-        protected override ResolvedTypeInfo ResolveType(ITypeReference typeInfo)
-        {
-            if (typeInfo == null)
+            public SqlTypeResolverContext() : base(CollectionFormatter.GetOrCreate("{0}"), TypeResolution.DefaultNullableFormatter.Instance)
             {
-                return null;
             }
 
-            if (typeInfo.HasStereotype("SQL Type Override"))
+            protected override IResolvedTypeInfo ResolveType(ITypeReference typeReference, INullableFormatter nullableFormatter)
             {
-                return new ResolvedTypeInfo(typeInfo.GetStereotypeProperty<string>("SQL Type Override", "Type Name"), true, typeInfo, null);
+                if (typeReference == null)
+                {
+                    return null;
+                }
+
+                if (typeReference.HasStereotype("SQL Type Override"))
+                {
+                    return ResolvedTypeInfo.Create(
+                        name: typeReference.GetStereotypeProperty<string>("SQL Type Override", "Type Name"),
+                        isPrimitive: true,
+                        typeReference: typeReference,
+                        template: null,
+                        nullableFormatter: null,
+                        collectionFormatter: null);
+                }
+
+                string result = null;
+                switch (typeReference.Element.Name)
+                {
+                    case "binary":
+                        result = "VARBINARY";
+                        break;
+                    case "bool":
+                        result = "BIT";
+                        break;
+                    case "byte":
+                        result = "TINYINT";
+                        break;
+                    case "char":
+                        result = "CHAR";
+                        break;
+                    case "date":
+                        result = "DATE";
+                        break;
+                    case "datetime":
+                        result = "DATETIME";
+                        break;
+                    case "datetimeoffset":
+                        result = "DATETIMEOFFSET";
+                        break;
+                    case "decimal":
+                        result = $"DECIMAL({typeReference.GetStereotypeProperty("Decimal Constraints", "Precision", "18")}, {typeReference.GetStereotypeProperty("Decimal Constraints", "Scale", "2")})";
+                        break;
+                    case "double":
+                        result = "FLOAT";
+                        break;
+                    case "float":
+                        result = "FLOAT";
+                        break;
+                    case "guid":
+                        result = "UNIQUEIDENTIFIER";
+                        break;
+                    case "int":
+                        result = "INT";
+                        break;
+                    case "long":
+                        result = "BIGINT";
+                        break;
+                    case "object":
+                        throw new Exception("Cannot convert type 'object' to a valid SQL Data Type");
+                    case "short":
+                        result = "SMALLINT";
+                        break;
+                    case "string":
+                        var type = typeReference.GetStereotypeProperty("Text Constraints", "SQL Data Type", "NVARCHAR").Replace("DEFAULT", "NVARCHAR");
+                        result = $"{type}({typeReference.GetStereotypeProperty("Text Constraints", "MaxLength", typeReference.GetStereotypeProperty("Text Constraints", "Max Length", "MAX"))})";
+                        break;
+                }
+
+                result += $" {(typeReference.IsNullable ? "NULL" : "NOT NULL")}";
+
+                return ResolvedTypeInfo.Create(
+                    name: result,
+                    isPrimitive: true,
+                    typeReference: typeReference,
+                    template: null,
+                    nullableFormatter: null,
+                    collectionFormatter: null);
             }
-
-            string result = null;
-            switch (typeInfo.Element.Name)
-            {
-                case "binary":
-                    result = "VARBINARY";
-                    break;
-                case "bool":
-                    result = "BIT";
-                    break;
-                case "byte":
-                    result = "TINYINT";
-                    break;
-                case "char":
-                    result = "CHAR";
-                    break;
-                case "date":
-                    result = "DATE";
-                    break;
-                case "datetime":
-                    result = "DATETIME";
-                    break;
-                case "datetimeoffset":
-                    result = "DATETIMEOFFSET";
-                    break;
-                case "decimal":
-                    result = $"DECIMAL({typeInfo.GetStereotypeProperty("Decimal Constraints", "Precision", "18")}, {typeInfo.GetStereotypeProperty("Decimal Constraints", "Scale", "2")})";
-                    break;
-                case "double":
-                    result = "FLOAT";
-                    break;
-                case "float":
-                    result = "FLOAT";
-                    break;
-                case "guid":
-                    result = "UNIQUEIDENTIFIER";
-                    break;
-                case "int":
-                    result = "INT";
-                    break;
-                case "long":
-                    result = "BIGINT";
-                    break;
-                case "object":
-                    throw new Exception("Cannot convert type 'object' to a valid SQL Data Type");
-                    break;
-                case "short":
-                    result = "SMALLINT";
-                    break;
-                case "string":
-                    var type = typeInfo.GetStereotypeProperty("Text Constraints", "SQL Data Type", "NVARCHAR").Replace("DEFAULT", "NVARCHAR");
-                    result = $"{type}({typeInfo.GetStereotypeProperty("Text Constraints", "MaxLength", typeInfo.GetStereotypeProperty("Text Constraints", "Max Length", "MAX"))})";
-                    break;
-            }
-
-            result += $" {(typeInfo.IsNullable ? "NULL" : "NOT NULL")}";
-
-            return new ResolvedTypeInfo(result, true, typeInfo, null);
         }
     }
 }
