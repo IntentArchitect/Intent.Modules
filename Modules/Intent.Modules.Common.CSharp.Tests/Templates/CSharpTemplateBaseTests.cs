e363556561cba6c0ed39bcdbf08709c019b20aaf
Jonathan Lydall <jonathan.lydall@intentarchitect.com>
2022-06-29 13:05:00 +0200

    AB#823 - GetTypeName default collection format not applying to type sources
    Unit test which reproduces of the problem.

--- Modules/Intent.Modules.Common.CSharp.Tests/Templates/CSharpTemplateBaseTests.cs
+++ Modules/Intent.Modules.Common.CSharp.Tests/Templates/CSharpTemplateBaseTests.cs
@@ -1,21 +1,86 @@
-﻿using System;
+﻿// ReSharper disable InconsistentNaming
+using System;
 using System.Collections.Generic;
-using System.Security.Cryptography;
-using System.Text;
 using Intent.Engine;
+using Intent.Metadata.Models;
 using Intent.Modules.Common.CSharp.Templates;
-using Intent.Modules.Common.Templates;
 using Intent.Templates;
 using NSubstitute;
 using Shouldly;
 using Xunit;
 
-// ReSharper disable InconsistentNaming
-
 namespace Intent.Modules.Common.CSharp.Tests.Templates
 {
     public class CSharpTemplateBaseTests
     {
+        public class AddTypeSource_String
+        {
+            [Fact]
+            public void ItShouldUseTheDefaultTypeCollectionFormat()
+            {
+                // Arrange
+                var template = new TestableCsharpTemplate();
+                template.SetDefaultTypeCollectionFormat("List<{0}>");
+                template.AddTypeSource(template.Id);
+
+                var typeReference = Substitute.For<ITypeReference>();
+                typeReference.Element.Returns(Substitute.For<IElement>());
+                typeReference.IsCollection.Returns(true);
+
+                // Act
+                var typeName = template.GetTypeName(typeReference);
+
+                // Assert
+                typeName.ShouldBe("List<TypeName>");
+            }
+
+            private class TestableCsharpTemplate : CSharpTemplateBase<object>
+            {
+                private static ITemplate _instance;
+
+                public TestableCsharpTemplate()
+                    : base(
+                        "1",
+                        GetOutputTarget(),
+                        Substitute.For<IMetadataModel>())
+                {
+                    if (_instance != null) throw new Exception();
+                    _instance = this;
+
+                    var customMetadata = new Dictionary<string, string>();
+                    foreach (var (key, value) in GetTemplateFileConfig().CustomMetadata)
+                    {
+                        customMetadata.Add(key, value);
+                    }
+
+                    var fileMetadata = Substitute.For<IFileMetadata>();
+                    fileMetadata.CustomMetadata.Returns(customMetadata);
+                    ConfigureFileMetadata(fileMetadata);
+                }
+
+                private static IOutputTarget GetOutputTarget()
+                {
+                    var ec = Substitute.For<ISoftwareFactoryExecutionContext>();
+                    ec.FindTemplateInstance(Arg.Any<string>(), Arg.Any<string>()).ReturnsForAnyArgs(_ => _instance);
+
+                    var outputTarget = Substitute.For<IOutputTarget>();
+                    outputTarget.ExecutionContext.Returns(ec);
+
+                    return outputTarget;
+                }
+
+                public override string TransformText()
+                {
+                    return string.Empty;
+                }
+
+                protected override CSharpFileConfig DefineFileConfig()
+                {
+                    return new CSharpFileConfig("TypeName", "Namespace");
+                }
+            }
+        }
+
         public class NormalizeNamespace_String
         {
             [Fact]
