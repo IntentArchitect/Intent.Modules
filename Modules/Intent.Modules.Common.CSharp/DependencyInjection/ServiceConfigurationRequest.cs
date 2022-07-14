26796ed1906a61dc89975ef5e166af557dff83c9
dandrejvv <dandrejvv@gmail.com>
2022-07-14 10:43:24 +0200

    Got better inspiration from the neighboring class for container registrations (now that I understand things better)
    AB#878

--- Modules/Intent.Modules.Common.CSharp/DependencyInjection/ServiceConfigurationRequest.cs
+++ Modules/Intent.Modules.Common.CSharp/DependencyInjection/ServiceConfigurationRequest.cs
@@ -2,42 +2,59 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using Intent.Modules.Common.Templates;
+using Intent.Templates;
 
 namespace Intent.Modules.Common.CSharp.DependencyInjection;
 
 public class ServiceConfigurationRequest
 {
+    private readonly List<ITemplateDependency> _templateDependencies = new List<ITemplateDependency>();
+    private readonly List<string> _requiredNamespaces = new List<string>();
+    
     private ServiceConfigurationRequest(
-        IntentTemplateBase sourceConfigurationTemplate,
         string extensionMethodName,
         IEnumerable<string> extensionMethodParameterList)
     {
-        SourceConfigurationTemplate = sourceConfigurationTemplate;
         ExtensionMethodName = extensionMethodName;
         ExtensionMethodParameterList = extensionMethodParameterList ?? Enumerable.Empty<string>();
     }
 
-    public IntentTemplateBase SourceConfigurationTemplate { get; }
     public string ExtensionMethodName { get; }
     public IEnumerable<string> ExtensionMethodParameterList { get; }
-
     public int Priority { get; private set; }
     public bool IsHandled { get; private set; }
+    public string Concern { get; private set; }
+    public IEnumerable<ITemplateDependency> TemplateDependencies => _templateDependencies;
+    public IEnumerable<string> RequiredNamespaces => _requiredNamespaces;
 
-    public static ServiceConfigurationRequest ForExtensionMethod(
-        IntentTemplateBase sourceConfigurationTemplate,
+    public static ServiceConfigurationRequest ToRegister(
         string extensionMethodName,
         params string[] extensionMethodParameterList)
     {
-        return new ServiceConfigurationRequest(
-            sourceConfigurationTemplate: sourceConfigurationTemplate, 
-            extensionMethodName: extensionMethodName,
-            extensionMethodParameterList: extensionMethodParameterList);
+        return new ServiceConfigurationRequest(extensionMethodName, extensionMethodParameterList);
     }
-
+    
     public ServiceConfigurationRequest WithPriority(int priority)
     {
         Priority = priority;
+        return this;
+    }
+    
+    public ServiceConfigurationRequest HasDependency(ITemplate template)
+    {
+        _templateDependencies.Add(TemplateDependency.OnTemplate(template));
+        return this;
+    }
+    
+    public ServiceConfigurationRequest RequiresUsingNamespaces(params string[] namespaces)
+    {
+        _requiredNamespaces.AddRange(namespaces);
+        return this;
+    }
+    
+    public ServiceConfigurationRequest ForConcern(string concern)
+    {
+        Concern = concern;
         return this;
     }
 
@@ -45,9 +62,9 @@ public class ServiceConfigurationRequest
     {
         IsHandled = true;
     }
-}
-
-public static class ServiceConfigurationParameterType
-{
-    public const string Configuration = "Microsoft.Extensions.Configuration.IConfiguration";
+    
+    public static class ParameterType
+    {
+        public const string Configuration = "Microsoft.Extensions.Configuration.IConfiguration";
+    }
 }
