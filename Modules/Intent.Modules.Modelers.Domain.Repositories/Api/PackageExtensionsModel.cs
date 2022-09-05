100ffea088061a45df514a2cc71b359ed0ebb3fd
Jonathan Lydall <jonathan.lydall@intentarchitect.com>
2022-09-05 05:58:56 +0200

    AB#1104 - Repository designer extension for domain designer

---
+++ Modules/Intent.Modules.Modelers.Domain.Repositories/Api/PackageExtensionsModel.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Intent.Metadata.Models;
+using Intent.Modelers.Domain.Api;
+using Intent.Modules.Common;
+using Intent.RoslynWeaver.Attributes;
+
+[assembly: DefaultIntentManaged(Mode.Fully)]
+[assembly: IntentTemplate("Intent.ModuleBuilder.Templates.Api.ApiPackageExtensionModel", Version = "1.0")]
+
+namespace Intent.Modelers.Domain.Repositories.Api
+{
+    [IntentManaged(Mode.Merge)]
+    public class PackageExtensionsModel : DomainPackageModel
+    {
+        [IntentManaged(Mode.Ignore)]
+        public PackageExtensionsModel(IPackage package) : base(package)
+        {
+        }
+
+        public IList<RepositoryModel> Repositories => UnderlyingPackage.ChildElements
+            .GetElementsOfType(RepositoryModel.SpecializationTypeId)
+            .Select(x => new RepositoryModel(x))
+            .ToList();
+
+    }
+}
