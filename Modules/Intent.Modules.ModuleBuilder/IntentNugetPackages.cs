312e103a4b08cea5410a91a298de7cdd790512cc
dandrejvv <dandrejvv@gmail.com>
2022-09-27 09:55:58 +0200

    Updated a nuget package version which would have caused a compilation error

--- Modules/Intent.Modules.ModuleBuilder/IntentNugetPackages.cs
+++ Modules/Intent.Modules.ModuleBuilder/IntentNugetPackages.cs
@@ -9,7 +9,7 @@ namespace Intent.Modules.ModuleBuilder
         public static NugetPackageInfo IntentPackager = new NugetPackageInfo("Intent.Packager", "3.3.0")
             .SpecifyAssetsBehaviour(new[] { "all" }, new[] { "runtime", "build", "native", "contentfiles", "analyzers", "buildtransitive" });
 
-        public static NugetPackageInfo IntentModulesCommon = new("Intent.Modules.Common", "3.3.10");
+        public static NugetPackageInfo IntentModulesCommon = new("Intent.Modules.Common", "3.3.11");
         public static NugetPackageInfo IntentModulesCommonTypes = new("Intent.Modules.Common.Types", "3.3.1");
     }
 }
