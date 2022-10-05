c3c8cf649e96422cf5a6bd8e29db091cd00b2323
dandrejvv <dandrejvv@gmail.com>
2022-10-05 13:52:49 +0200

    Don't consider Abstract types as Aggregate Roots

--- Modules/Intent.Modules.Modelers.Domain/Api/Extensions/ClassModelAssociationExtensions.cs
+++ Modules/Intent.Modules.Modelers.Domain/Api/Extensions/ClassModelAssociationExtensions.cs
@@ -4,11 +4,15 @@ namespace Intent.Modelers.Domain.Api;
 
 public static class ClassModelAssociationExtensions
 {
+    /// <summary>
+    /// Is the <see cref="ClassModel"/> an Aggregate Root?
+    /// An Aggregate Root is owned by nothing and can be instantiated.
+    /// </summary>
     public static bool IsAggregateRoot(this ClassModel classModel)
     {
         var owningAssociations = classModel.AssociationEnds()
             .Where(x => x.IsSourceEnd() && !x.IsCollection && !x.IsNullable)
             .ToArray();
-        return !owningAssociations.Any();
+        return !owningAssociations.Any() && !classModel.IsAbstract;
     }
 }
--- Modules/Intent.Modules.Modelers.Domain/release-notes.md
+++ Modules/Intent.Modules.Modelers.Domain/release-notes.md
@@ -1,4 +1,8 @@
-﻿### Version 3.3.6
+﻿### Version 3.3.8
+
+- Update: `IsAggregateRoot()` will no longer consider `IsAbstract` models as Aggregate Roots. 
+
+### Version 3.3.6
 
  - Domain settings: Allows specifiying default naming conventions for Attributes (e.g. `camel-case` or `pascal-case`).
  - Validations to warn of duplicate Attributes and Classes names.
