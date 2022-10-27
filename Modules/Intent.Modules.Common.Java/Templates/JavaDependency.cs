3050b3b520d4111b0d5ca0425766620f6b35c09f
Jonathan Lydall <jonathan.lydall@intentarchitect.com>
2022-10-27 17:30:14 +0200

    AB#1348 - Updates to Java modules
    Add ability to specify "optional" for JavaDependency.

--- Modules/Intent.Modules.Common.Java/Templates/JavaDependency.cs
+++ Modules/Intent.Modules.Common.Java/Templates/JavaDependency.cs
@@ -1,15 +1,46 @@
 ﻿namespace Intent.Modules.Common.Java.Templates
 {
+    /// <summary>
+    /// Details of a Java Maven dependency.
+    /// </summary>
     public class JavaDependency
     {
-        public JavaDependency(string groupId, string artifactId, string version = null)
+        /// <summary>
+        /// Creates a new instance of <see cref="JavaDependency"/>.
+        /// </summary>
+        public JavaDependency(string groupId, string artifactId, string version = null, bool optional = false)
         {
             GroupId = groupId;
             ArtifactId = artifactId;
             Version = version;
+            Optional = optional;
         }
+
+        /// <summary>
+        /// Creates a new instance of <see cref="JavaDependency"/>.
+        /// </summary>
+        public JavaDependency(string groupId, string artifactId, string version = null) : this(groupId, artifactId, version, false)
+        {
+        }
+
+        /// <summary>
+        /// The groupId of the dependency.
+        /// </summary>
         public string GroupId { get; set; }
+
+        /// <summary>
+        /// The groupId of the dependency.
+        /// </summary>
         public string ArtifactId { get; set; }
+
+        /// <summary>
+        /// The version of the dependency.
+        /// </summary>
         public string Version { get; set; }
+
+        /// <summary>
+        /// Whether or not the dependency is optional.
+        /// </summary>
+        public bool Optional { get; set; }
     }
 }
