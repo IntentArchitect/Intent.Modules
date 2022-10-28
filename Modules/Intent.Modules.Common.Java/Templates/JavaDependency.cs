65ad533835e2a82dc9d3de094f77fef07aae8daf
Jonathan Lydall <jonathan.lydall@intentarchitect.com>
2022-10-28 15:47:06 +0200

    Improve backwards compatibility.

--- Modules/Intent.Modules.Common.Java/Templates/JavaDependency.cs
+++ Modules/Intent.Modules.Common.Java/Templates/JavaDependency.cs
@@ -1,4 +1,6 @@
-﻿namespace Intent.Modules.Common.Java.Templates
+﻿using Intent.SdkEvolutionHelpers;
+
+namespace Intent.Modules.Common.Java.Templates
 {
     /// <summary>
     /// Details of a Java Maven dependency.
@@ -8,7 +10,7 @@
         /// <summary>
         /// Creates a new instance of <see cref="JavaDependency"/>.
         /// </summary>
-        public JavaDependency(string groupId, string artifactId, string version = null, bool optional = false)
+        public JavaDependency(string groupId, string artifactId, string version, bool optional)
         {
             GroupId = groupId;
             ArtifactId = artifactId;
@@ -19,7 +21,16 @@
         /// <summary>
         /// Creates a new instance of <see cref="JavaDependency"/>.
         /// </summary>
+        [FixFor_Version4("Make a single constructor where version has a default value of null and optional has a default value of false")]
         public JavaDependency(string groupId, string artifactId, string version = null) : this(groupId, artifactId, version, false)
+        {
+        }
+
+        /// <summary>
+        /// Creates a new instance of <see cref="JavaDependency"/>.
+        /// </summary>
+        [FixFor_Version4("Make a single constructor where version has a default value of null and optional has a default value of false")]
+        public JavaDependency(string groupId, string artifactId, bool optional) : this(groupId, artifactId, null, optional)
         {
         }
 
