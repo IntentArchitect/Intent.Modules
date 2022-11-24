2e2746c5695517d28a0a671cbcbef7f58b6138e8
Jonathan Lydall <jonathan.lydall@intentarchitect.com>
2022-11-24 20:55:50 +0200

    Event handlers to auto-sync keys on Items.

--- Modules/Intent.Modules.Modelers.AWS.DynamoDB/ApiMetadataProviderExtensions.cs
+++ Modules/Intent.Modules.Modelers.AWS.DynamoDB/ApiMetadataProviderExtensions.cs
@@ -12,24 +12,10 @@ namespace Intent.Modelers.AWS.DynamoDB.Api
     public static class ApiMetadataProviderExtensions
     {
 
-        public static IList<DynamoDBItemModel> GetDynamoDBItemModels(this IDesigner designer)
-        {
-            return designer.GetElementsOfType(DynamoDBItemModel.SpecializationTypeId)
-                .Select(x => new DynamoDBItemModel(x))
-                .ToList();
-        }
-
         public static IList<DynamoDBTableModel> GetDynamoDBTableModels(this IDesigner designer)
         {
             return designer.GetElementsOfType(DynamoDBTableModel.SpecializationTypeId)
                 .Select(x => new DynamoDBTableModel(x))
-                .ToList();
-        }
-
-        public static IList<MapAttributeModel> GetMapAttributeModels(this IDesigner designer)
-        {
-            return designer.GetElementsOfType(MapAttributeModel.SpecializationTypeId)
-                .Select(x => new MapAttributeModel(x))
                 .ToList();
         }
 
