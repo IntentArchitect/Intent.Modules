0cdae5526fd9446491bfab38dae48a0168ee22bc
garethbaars <gareth.baars@intentarchitect.com>
2022-12-08 18:09:24 +0100

    Update Module Builder's Common nuget dependency

--- Modules/Intent.Modules.ModuleBuilder/IntentNugetPackages.cs
+++ Modules/Intent.Modules.ModuleBuilder/IntentNugetPackages.cs
@@ -9,7 +9,7 @@ namespace Intent.Modules.ModuleBuilder
         public static NugetPackageInfo IntentPackager = new NugetPackageInfo("Intent.Packager", "3.4.0-pre.0")
             .SpecifyAssetsBehaviour(new[] { "all" }, new[] { "runtime", "build", "native", "contentfiles", "analyzers", "buildtransitive" });
 
-        public static NugetPackageInfo IntentModulesCommon = new("Intent.Modules.Common", "3.3.16-pre.0");
+        public static NugetPackageInfo IntentModulesCommon = new("Intent.Modules.Common", "3.3.16");
         public static NugetPackageInfo IntentModulesCommonTypes = new("Intent.Modules.Common.Types", "3.3.6");
     }
 }
