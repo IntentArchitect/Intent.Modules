8f50206f94f160b2c070f7e7439298e0c90a1073
Jonathan Lydall <jonathan.lydall@intentarchitect.com>
2023-01-14 12:15:26 +0200

    Remove suffixes from resource display names.

---
+++ Modules/Intent.Modules.Modelers.AWS.DynamoDB/Api/Extensions/EntityModelExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+using Intent.Modelers.AWS.DynamoDB.Api;
+
+namespace Intent.Modules.Modelers.AWS.DynamoDB.Api;
+
+public static class EntityModelExtensions
+{
+    /// <summary>
+    /// Is the <see cref="EntityModel"/> an Aggregate Root? An Aggregate Root is owned by nothing.
+    /// </summary>
+    public static bool IsAggregateRoot(this EntityModel entity)
+    {
+        return !entity.AssociationEnds()
+            .Any(x => x.IsSourceEnd() && !x.IsCollection && !x.IsNullable);
+    }
+}
