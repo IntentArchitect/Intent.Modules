85b8b5ee36444fd3c3872731ca372c5e2de37785
dandrejvv <dandrejvv@gmail.com>
2023-02-07 17:15:46 +0200

    Was missing proper inheritance check on PKs
    AB#1728

--- DesignerMacros/rdbms_association_key_on-changed.ts
+++ DesignerMacros/rdbms_association_key_on-changed.ts
@@ -6,36 +6,21 @@ if (application?.getSettings("ac0a788e-d8b3-4eea-b56d-538608f1ded9")?.getField("
     return;
 }
 
-function requiresForeignKey(associationEnd)
-{
-    return isManyToVariantsOfOne(associationEnd) || isSelfReferencingZeroToOne(associationEnd);
-}
-
-function isManyToVariantsOfOne(associationEnd)
-{
-    return !associationEnd.typeReference.isCollection && associationEnd.getOtherEnd().typeReference.isCollection;
-}
-
-function isSelfReferencingZeroToOne(associationEnd)
-{
-    return !associationEnd.typeReference.isCollection && associationEnd.typeReference.isNullable && 
-            associationEnd.typeReference.typeId == associationEnd.getOtherEnd().typeReference.typeId;
-}
-
 let sourceType = lookup(association.getOtherEnd().typeReference.typeId);
 let targetType = lookup(association.typeReference.typeId);
 
 if (sourceType && targetType) {
 
     if (requiresForeignKey(association) && sourceType.getMetadata("auto-manage-keys") != "false") {
-        let pks = targetType.getChildren("Attribute").filter(x => x.getStereotype("Primary Key") != null);
-        pks.forEach((pk, index) => {
-        let fk = sourceType.getChildren().filter(x => x.getMetadata("association") == association.id)[index] ||
+        let primaryKeyDict = getPrimaryKeysWithMapPath(targetType);
+        let primaryKeysLen = Object.values(primaryKeyDict).length;
+        Object.values(primaryKeyDict).forEach((pk, index) => {
+            let fk = sourceType.getChildren().filter(x => x.getMetadata("association") == association.id)[index] ||
                  createElement("Attribute", "", sourceType.id);
             // This check to avoid a loop where the Domain script is updating the convensions and this keeps renaming it back.
-            if (fk.getName().toLocaleLowerCase() !== `${ toCamelCase(association.getName())}${toPascalCase(pk.getName())}`.toLocaleLowerCase()) {
+            if (fk.getName().toLocaleLowerCase() !== `${ toCamelCase(association.getName())}${toPascalCase(pk.name)}`.toLocaleLowerCase()) {
                 if (!fk.hasMetadata("fk-original-name") || (fk.getMetadata("fk-original-name") == fk.getName())) {
-                    fk.setName(`${ toCamelCase(association.getName())}${toPascalCase(pk.getName())}`);
+                    fk.setName(`${ toCamelCase(association.getName())}${toPascalCase(pk.name)}`);
                     fk.setMetadata("fk-original-name", fk.getName());
                 }
             }
@@ -49,26 +34,28 @@ if (sourceType && targetType) {
             }
             fkStereotype.getProperty("Association").setValue(association.id);
 
-            fk.typeReference.setType(pk.typeReference.typeId);
+            fk.typeReference.setType(pk.typeId);
             fk.typeReference.setIsNullable(association.typeReference.isNullable);
         });
-        sourceType.getChildren().filter(x => x.getMetadata("association") == association.id).forEach((x, index) => {
-            if (index >= pks.length)
-                x.delete();
-        })
+        sourceType.getChildren().filter(x => x.getMetadata("association") == association.id).forEach((attr, index) => {
+            if (index >= primaryKeysLen) {
+                attr.delete();
+            }
+        });
         if (targetType.id !== sourceType.id && targetType.getMetadata("auto-manage-keys") != "false") {
             targetType.getChildren().filter(x => x.getMetadata("association") == association.id).forEach(x => x.delete());
         }
     }
     else if (requiresForeignKey(association.getOtherEnd()) && targetType.getMetadata("auto-manage-keys") != "false") {
-        let pks = sourceType.getChildren("Attribute").filter(x => x.getStereotype("Primary Key") != null);
-        pks.forEach((pk, index) => {
-        let fk = targetType.getChildren().filter(x => x.getMetadata("association") == association.id)[index] ||
+        let primaryKeyDict = getPrimaryKeysWithMapPath(sourceType);
+        let primaryKeysLen = Object.values(primaryKeyDict).length;
+        Object.values(primaryKeyDict).forEach((pk, index) => {
+            let fk = targetType.getChildren().filter(x => x.getMetadata("association") == association.id)[index] ||
                  createElement("Attribute", "", targetType.id);
             // This check to avoid a loop where the Domain script is updating the convensions and this keeps renaming it back.
-            if (fk.getName().toLocaleLowerCase() !== `${ toCamelCase(association.getOtherEnd().getName()) }${toPascalCase(pk.getName())}`.toLocaleLowerCase()) {
+            if (fk.getName().toLocaleLowerCase() !== `${ toCamelCase(association.getOtherEnd().getName()) }${toPascalCase(pk.name)}`.toLocaleLowerCase()) {
                 if (!fk.hasMetadata("fk-original-name") || (fk.getMetadata("fk-original-name") == fk.getName())) {
-                    fk.setName(`${ toCamelCase(association.getOtherEnd().getName()) }${toPascalCase(pk.getName())}`);
+                    fk.setName(`${ toCamelCase(association.getOtherEnd().getName()) }${toPascalCase(pk.name)}`);
                     fk.setMetadata("fk-original-name", fk.getName());
                 }
             }
@@ -82,13 +69,14 @@ if (sourceType && targetType) {
             }
             fkStereotype.getProperty("Association").setValue(association.id);
 
-            fk.typeReference.setType(pk.typeReference.typeId);
+            fk.typeReference.setType(pk.typeId);
             fk.typeReference.setIsNullable(association.getOtherEnd().typeReference.isNullable);
         });
-        targetType.getChildren().filter(x => x.getMetadata("association") == association.id).forEach((x, index) => {
-            if (index >= pks.length)
-                x.delete();
-        })
+        targetType.getChildren().filter(x => x.getMetadata("association") == association.id).forEach((attr, index) => {
+            if (index >= primaryKeysLen) {
+                attr.delete();
+            }
+        });
         if (sourceType.id !== targetType.id && sourceType.getMetadata("auto-manage-keys") != "false") {
             sourceType.getChildren().filter(x => x.getMetadata("association") == association.id).forEach(x => x.delete());
         }
@@ -100,6 +88,80 @@ if (sourceType && targetType) {
         if (sourceType.getMetadata("auto-manage-keys") != "false") {
             sourceType.getChildren().filter(x => x.getMetadata("association") == association.id).forEach(x => x.delete());
         }
+    }
+}
+
+function requiresForeignKey(associationEnd : MacroApi.Context.IAssociationApi) : boolean {
+    return isManyToVariantsOfOne(associationEnd) || isSelfReferencingZeroToOne(associationEnd);
+}
+
+function isManyToVariantsOfOne(associationEnd : MacroApi.Context.IAssociationApi) : boolean {
+    return !associationEnd.typeReference.isCollection && associationEnd.getOtherEnd().typeReference.isCollection;
+}
+
+function isSelfReferencingZeroToOne(associationEnd : MacroApi.Context.IAssociationApi) : boolean {
+    return !associationEnd.typeReference.isCollection && associationEnd.typeReference.isNullable && 
+            associationEnd.typeReference.typeId == associationEnd.getOtherEnd().typeReference.typeId;
+}
+
+interface IAttributeWithMapPath {
+    id: string,
+    name: string,
+    typeId: string,
+    mapPath: string[],
+    isNullable: boolean,
+    isCollection: boolean
+};
+
+function getPrimaryKeysWithMapPath(entity : MacroApi.Context.IElementApi) {
+    let keydict : { [characterName: string]: IAttributeWithMapPath } = Object.create(null);
+    let keys = entity.getChildren("Attribute").filter(x => x.hasStereotype("Primary Key"));
+    
+    let generalizations = entity.getAssociations("Generalization").filter(x => x.isTargetEnd());
+    // There is a problem with execution order where this script executes before
+    // the generalization script had a chance to potentially remove a PK attribute
+    // and so I have to perform an inheritance check and ignore any PKs on derived classes.
+    if (generalizations.length == 0) {
+        keys.forEach(key => keydict[key.id] = { 
+            id: key.id, 
+            name: key.getName(), 
+            typeId: key.typeReference.typeId,
+            mapPath: [key.id],
+            isNullable: false,
+            isCollection: false
+        });
+    }
+
+    traverseInheritanceHierarchyForPrimaryKeys(keydict, entity, []);
+
+    return keydict;
+
+    function traverseInheritanceHierarchyForPrimaryKeys(
+        keydict: { [characterName: string]: IAttributeWithMapPath }, 
+        curEntity: MacroApi.Context.IElementApi, 
+        generalizationStack) {
+        if (!curEntity) {
+            return;
+        }
+        let generalizations = curEntity.getAssociations("Generalization").filter(x => x.isTargetEnd());
+        if (generalizations.length == 0) {
+            return;
+        }
+        let generalization = generalizations[0];
+        generalizationStack.push(generalization.id);
+        let nextEntity = generalization.typeReference.getType();
+        let baseKeys = nextEntity.getChildren("Attribute").filter(x => x.hasStereotype("Primary Key"));
+        baseKeys.forEach(key => { 
+            keydict[key.id] = { 
+                id: key.id, 
+                name: key.getName(),
+                typeId: key.typeReference.typeId,
+                mapPath: generalizationStack.concat([key.id]),
+                isNullable: key.typeReference.isNullable,
+                isCollection: key.typeReference.isCollection
+            };
+        });
+        traverseInheritanceHierarchyForPrimaryKeys(keydict, nextEntity, generalizationStack);
     }
 }
 
