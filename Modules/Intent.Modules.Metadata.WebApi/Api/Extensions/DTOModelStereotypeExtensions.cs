a929cba1aa88e1193947e672d7d463924b198fbf
Joel Turner <joel.turner@intentarchitect.com>
2023-03-15 09:20:32 +0200

    AB#1940 - Serialization Config for DTOS

---
+++ Modules/Intent.Modules.Metadata.WebApi/Api/Extensions/DTOModelStereotypeExtensions.cs
@@ -0,0 +1,178 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Intent.Metadata.Models;
+using Intent.Modelers.Services.Api;
+using Intent.Modules.Common;
+using Intent.RoslynWeaver.Attributes;
+
+[assembly: DefaultIntentManaged(Mode.Fully)]
+[assembly: IntentTemplate("Intent.ModuleBuilder.Templates.Api.ApiElementModelExtensions", Version = "1.0")]
+
+namespace Intent.Metadata.WebApi.Api
+{
+    public static class DTOModelStereotypeExtensions
+    {
+        public static SerializationSettings GetSerializationSettings(this DTOModel model)
+        {
+            var stereotype = model.GetStereotype("Serialization Settings");
+            return stereotype != null ? new SerializationSettings(stereotype) : null;
+        }
+
+
+        public static bool HasSerializationSettings(this DTOModel model)
+        {
+            return model.HasStereotype("Serialization Settings");
+        }
+
+        public static bool TryGetSerializationSettings(this DTOModel model, out SerializationSettings stereotype)
+        {
+            if (!HasSerializationSettings(model))
+            {
+                stereotype = null;
+                return false;
+            }
+
+            stereotype = new SerializationSettings(model.GetStereotype("Serialization Settings"));
+            return true;
+        }
+
+        public class SerializationSettings
+        {
+            private IStereotype _stereotype;
+
+            public SerializationSettings(IStereotype stereotype)
+            {
+                _stereotype = stereotype;
+            }
+
+            public string Name => _stereotype.Name;
+
+            public FieldNamingConventionOptions FieldNamingConvention()
+            {
+                return new FieldNamingConventionOptions(_stereotype.GetProperty<string>("Field Naming Convention"));
+            }
+
+            public NamingConventionOptions NamingConvention()
+            {
+                return new NamingConventionOptions(_stereotype.GetProperty<string>("Naming Convention"));
+            }
+
+            public string CustomName()
+            {
+                return _stereotype.GetProperty<string>("Custom Name");
+            }
+
+            public class FieldNamingConventionOptions
+            {
+                public readonly string Value;
+
+                public FieldNamingConventionOptions(string value)
+                {
+                    Value = value;
+                }
+
+                public FieldNamingConventionOptionsEnum AsEnum()
+                {
+                    switch (Value)
+                    {
+                        case "Camel Case":
+                            return FieldNamingConventionOptionsEnum.CamelCase;
+                        case "Kebab Case":
+                            return FieldNamingConventionOptionsEnum.KebabCase;
+                        case "Pascal Case":
+                            return FieldNamingConventionOptionsEnum.PascalCase;
+                        case "Snake Case":
+                            return FieldNamingConventionOptionsEnum.SnakeCase;
+                        default:
+                            throw new ArgumentOutOfRangeException();
+                    }
+                }
+
+                public bool IsCamelCase()
+                {
+                    return Value == "Camel Case";
+                }
+                public bool IsKebabCase()
+                {
+                    return Value == "Kebab Case";
+                }
+                public bool IsPascalCase()
+                {
+                    return Value == "Pascal Case";
+                }
+                public bool IsSnakeCase()
+                {
+                    return Value == "Snake Case";
+                }
+            }
+
+            public enum FieldNamingConventionOptionsEnum
+            {
+                CamelCase,
+                KebabCase,
+                PascalCase,
+                SnakeCase
+            }
+            public class NamingConventionOptions
+            {
+                public readonly string Value;
+
+                public NamingConventionOptions(string value)
+                {
+                    Value = value;
+                }
+
+                public NamingConventionOptionsEnum AsEnum()
+                {
+                    switch (Value)
+                    {
+                        case "Camel Case":
+                            return NamingConventionOptionsEnum.CamelCase;
+                        case "Kebab Case":
+                            return NamingConventionOptionsEnum.KebabCase;
+                        case "Pascal Case":
+                            return NamingConventionOptionsEnum.PascalCase;
+                        case "Snake Case":
+                            return NamingConventionOptionsEnum.SnakeCase;
+                        case "Custom":
+                            return NamingConventionOptionsEnum.Custom;
+                        default:
+                            throw new ArgumentOutOfRangeException();
+                    }
+                }
+
+                public bool IsCamelCase()
+                {
+                    return Value == "Camel Case";
+                }
+                public bool IsKebabCase()
+                {
+                    return Value == "Kebab Case";
+                }
+                public bool IsPascalCase()
+                {
+                    return Value == "Pascal Case";
+                }
+                public bool IsSnakeCase()
+                {
+                    return Value == "Snake Case";
+                }
+                public bool IsCustom()
+                {
+                    return Value == "Custom";
+                }
+            }
+
+            public enum NamingConventionOptionsEnum
+            {
+                CamelCase,
+                KebabCase,
+                PascalCase,
+                SnakeCase,
+                Custom
+            }
+        }
+
+    }
+}
--- Modules/Intent.Modules.Metadata.WebApi/release-notes.md
+++ Modules/Intent.Modules.Metadata.WebApi/release-notes.md
@@ -1,11 +1,15 @@
+### Version 3.3.11
+
+- Added the `Serialization Settings` stereotype, which can be applied to `DTO`s and `DTO-Field`s. This is presently only supported by the `Intent.Application.Dtos` module.
+
 ### Version 3.3.9
 
 - Update: Http Settings is no longer `Always` applied. This is to ensure that Modules installing certain Technology stacks can control whether these Stereotypes are applied or not. 
 
 ### Version 3.3.8
 
- - New: Return Type Mediatype added for service operations to indicate how their response payload should be returned.
+- New: Return Type Mediatype added for service operations to indicate how their response payload should be returned.
 
 ### Version 3.3.7
 
- - Added validation on Header names since having spaces is considered invalid for user defined headers.
+- Added validation on Header names since having spaces is considered invalid for user defined headers.
