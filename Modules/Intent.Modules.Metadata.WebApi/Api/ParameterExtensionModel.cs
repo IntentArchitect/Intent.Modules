b31d3e4c91d0d6b64b467cd8dad1f37c19a4b7c7
dandrejvv <dandrejvv@gmail.com>
2023-04-13 01:06:59 +0200

    Preparing to publish Common Modules. Part 3.

---
+++ Modules/Intent.Modules.Metadata.WebApi/Api/ParameterExtensionModel.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Intent.Metadata.Models;
+using Intent.Modelers.Services.Api;
+using Intent.Modules.Common;
+using Intent.RoslynWeaver.Attributes;
+
+[assembly: DefaultIntentManaged(Mode.Fully)]
+[assembly: IntentTemplate("Intent.ModuleBuilder.Templates.Api.ApiElementExtensionModel", Version = "1.0")]
+
+namespace Intent.Metadata.WebApi.Api
+{
+    [IntentManaged(Mode.Fully, Signature = Mode.Fully)]
+    public class ParameterExtensionModel : ParameterModel
+    {
+        [IntentManaged(Mode.Fully, Body = Mode.Ignore)]
+        public ParameterExtensionModel(IElement element) : base(element)
+        {
+        }
+
+    }
+}
