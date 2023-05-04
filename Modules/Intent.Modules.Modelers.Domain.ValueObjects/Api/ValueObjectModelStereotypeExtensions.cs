632cbbf47df9549d9d20138dc0a03e5a358f872b
dandrejvv <dandrejvv@gmail.com>
2023-05-04 17:49:16 +0200

    Value Object can now indicate Serialization settings

---
+++ Modules/Intent.Modules.Modelers.Domain.ValueObjects/Api/ValueObjectModelStereotypeExtensions.cs
@@ -0,0 +1,88 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Intent.Metadata.Models;
+using Intent.Modules.Common;
+using Intent.RoslynWeaver.Attributes;
+
+[assembly: DefaultIntentManaged(Mode.Fully)]
+[assembly: IntentTemplate("Intent.ModuleBuilder.Templates.Api.ApiElementModelExtensions", Version = "1.0")]
+
+namespace Intent.Modelers.Domain.ValueObjects.Api
+{
+    public static class ValueObjectModelStereotypeExtensions
+    {
+        public static SerializationSettings GetSerializationSettings(this ValueObjectModel model)
+        {
+            var stereotype = model.GetStereotype("Serialization Settings");
+            return stereotype != null ? new SerializationSettings(stereotype) : null;
+        }
+
+
+        public static bool HasSerializationSettings(this ValueObjectModel model)
+        {
+            return model.HasStereotype("Serialization Settings");
+        }
+
+        public static bool TryGetSerializationSettings(this ValueObjectModel model, out SerializationSettings stereotype)
+        {
+            if (!HasSerializationSettings(model))
+            {
+                stereotype = null;
+                return false;
+            }
+
+            stereotype = new SerializationSettings(model.GetStereotype("Serialization Settings"));
+            return true;
+        }
+
+        public class SerializationSettings
+        {
+            private IStereotype _stereotype;
+
+            public SerializationSettings(IStereotype stereotype)
+            {
+                _stereotype = stereotype;
+            }
+
+            public string Name => _stereotype.Name;
+
+            public TypeOptions Type()
+            {
+                return new TypeOptions(_stereotype.GetProperty<string>("Type"));
+            }
+
+            public class TypeOptions
+            {
+                public readonly string Value;
+
+                public TypeOptions(string value)
+                {
+                    Value = value;
+                }
+
+                public TypeOptionsEnum AsEnum()
+                {
+                    switch (Value)
+                    {
+                        case "JSON":
+                            return TypeOptionsEnum.JSON;
+                        default:
+                            throw new ArgumentOutOfRangeException();
+                    }
+                }
+
+                public bool IsJSON()
+                {
+                    return Value == "JSON";
+                }
+            }
+
+            public enum TypeOptionsEnum
+            {
+                JSON
+            }
+        }
+
+    }
+}
--- Modules/Intent.Modules.Modelers.Domain.ValueObjects/release-notes.md
+++ Modules/Intent.Modules.Modelers.Domain.ValueObjects/release-notes.md
@@ -1,3 +1,7 @@
+### Version 3.4.0
+
+- New: Serialization Settings stereotype added.
+
 ### Version 3.3.1
 
 - Fixed: Parameters not able to reference Value Objects.
