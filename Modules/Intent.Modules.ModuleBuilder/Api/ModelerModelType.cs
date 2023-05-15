06b987f3d6a666f5026d5622ab3a0ce291068429
Gareth Baars <gareth.baars@intentarchitect.com>
2023-05-15 05:38:04 +0200

    ModuleBuilder 3.6.4-pre.0 - Added support for Templates to target Type-Definition model types. Will respect `C#` stereotype's `Namespace` setting.

--- Modules/Intent.Modules.ModuleBuilder/Api/ModelerModelType.cs
+++ Modules/Intent.Modules.ModuleBuilder/Api/ModelerModelType.cs
@@ -18,6 +18,7 @@ namespace Intent.ModuleBuilder.Api
                 element.SpecializationType != "Association Source End Settings" &&
                 element.SpecializationType != "Association Destination End Settings" &&
                 element.SpecializationType != "Element Extension" &&
+                element.SpecializationType != "Type-Definition" &&
                 element.SpecializationType != "Package Extension")
             {
                 throw new InvalidOperationException($"Cannot load {nameof(ModelerModelType)} from element of type {element.SpecializationType}");
@@ -27,8 +28,8 @@ namespace Intent.ModuleBuilder.Api
         }
 
         public string Id => _element.Id;
-        public string ClassName => $"{_element.Name.ToCSharpIdentifier()}Model";
-        public string Namespace => ParentModule.ApiNamespace;
+        public string ClassName => _element.SpecializationType == "Type-Definition" ? _element.Name.ToCSharpIdentifier() : $"{_element.Name.ToCSharpIdentifier()}Model";
+        public string Namespace => _element.GetStereotype("C#")?.GetProperty<string>("Namespace") ?? ParentModule.ApiNamespace;
         public string FullyQualifiedName => $"{Namespace}.{ClassName}";
 
         public IntentModuleModel ParentModule => new IntentModuleModel(_element.Package);
