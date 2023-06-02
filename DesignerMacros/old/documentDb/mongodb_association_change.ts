d13d5a1e4141b062cb730c226df6f5db82ff9165
Joel Turner <joel.turner@intentarchitect.com>
2023-06-02 14:14:22 +0200

    Mongo Designer Fix

--- DesignerMacros/old/documentDb/mongodb_association_change.ts
+++ DesignerMacros/old/documentDb/mongodb_association_change.ts
@@ -1,6 +1,10 @@
 (async () => {
 // This script was made using a Typescript source. Don't edit this script directly.
 {
+    //This happens while linking the association i.e. only 1 end attached.
+    if (!association.typeReference.getType()){
+        return;
+    }
     let sourceTarget = association.getOtherEnd().typeReference.getType();
 
     const documentStoreId = "8b68020c-6652-484b-85e8-6c33e1d8031f";
@@ -8,7 +12,7 @@
         return;
     }
 
-    updatePrimaryKey(sourceTarget);
+    updatePrimaryKey(association.typeReference.getType());
     updateForeignKeys(association);
 }
 
