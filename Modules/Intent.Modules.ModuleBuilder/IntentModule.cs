19dd05d09ee261f7f159f9d4ddfe7e73b97e08bf
dandrejvv <dandrejvv@gmail.com>
2023-08-21 16:55:13 +0200

    Updated which module versions to install

--- Modules/Intent.Modules.ModuleBuilder/IntentModule.cs
+++ Modules/Intent.Modules.ModuleBuilder/IntentModule.cs
@@ -3,7 +3,7 @@
     public class IntentModule
     {
         public static readonly IntentModule IntentRoslynWeaver = new("Intent.OutputManager.RoslynWeaver", "4.1.1");
-        public static readonly IntentModule IntentCommon = new("Intent.Common", "3.4.3-pre.0");
+        public static readonly IntentModule IntentCommon = new("Intent.Common", "3.4.3");
         public static readonly IntentModule IntentCommonTypes = new("Intent.Common.Types", "3.3.9");
 
         public IntentModule(string name, string version)
