2ac4c44096452e9091c72fe0a7616316293dbc33
Gareth Baars <gareth.baars@intentarchitect.com>
2023-09-03 08:42:13 +0200

    Services.DomainInteractions improvements

--- Modules/Intent.Modules.Modelers.Services.DomainInteractions/Api/CommandExtensionModel.cs
+++ Modules/Intent.Modules.Modelers.Services.DomainInteractions/Api/CommandExtensionModel.cs
@@ -19,5 +19,10 @@ namespace Intent.Modelers.Services.DomainInteractions.Api
         {
         }
 
+        public IList<ProcessingActionModel> ProcessingActions => _element.ChildElements
+            .GetElementsOfType(ProcessingActionModel.SpecializationTypeId)
+            .Select(x => new ProcessingActionModel(x))
+            .ToList();
+
     }
 }
