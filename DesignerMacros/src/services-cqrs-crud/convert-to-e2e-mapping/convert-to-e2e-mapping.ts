6f0d64082e1e4e00f2fae5ed8168c449a8bb9083
Gareth Baars <gareth.baars@intentarchitect.com>
2023-09-30 18:53:30 +0200

    Mapping improvements

--- DesignerMacros/src/services-cqrs-crud/convert-to-e2e-mapping/convert-to-e2e-mapping.ts
+++ DesignerMacros/src/services-cqrs-crud/convert-to-e2e-mapping/convert-to-e2e-mapping.ts
@@ -5,18 +5,21 @@ if (command.isMapped()) {
     let entity = command.getMapping().getElement();
     if (command.getName().startsWith("Create")) {
         let action = createAssociation("Create Entity Action", command.id, entity.id);
-        let mapping = action.createMapping(command.id, entity.id);
+        let mapping = action.createMapping(command.id, entity.getParent("Class")?.id ?? entity.id);
+        mapping.addMappedEnd([command.id], [entity.id]);
         mapContract(command, [command.id], [entity.id], mapping);
     } else if (command.getName().startsWith("Update")) {
         let entity = command.getMapping().getElement();
         let action = createAssociation("Update Entity Action", command.id, entity.id);
-        let mapping = action.createMapping(command.id, entity.id);
+
+        let queryMapping = action.createMapping(command.id, entity.getParent("Class")?.id ?? entity.id, "25f25af9-c38b-4053-9474-b0fabe9d7ea7"); // Query Entity Mapping
         let idField = command.getChildren("DTO-Field").find(x => !x.isMapped() && (x.getName() == "Id" || x.getName() == `${entity.getName()}Id`));
         let entityPk = entity.getChildren("Attribute").find(x => x.hasStereotype("Primary Key"));
         if (idField && entityPk) {
-            mapping.addMappedEnd([idField.id], [entityPk.id]);
+            queryMapping.addMappedEnd([idField.id], [entityPk.id]);
         }
-        mapContract(command, [command.id], [entity.id], mapping);
+        let updateMapping = action.createMapping(command.id, entity.getParent("Class")?.id ?? entity.id, "01721b1a-a85d-4320-a5cd-8bd39247196a"); // Update Entity Mapping
+        mapContract(command, [command.id], [entity.id], updateMapping);
     } else if (command.getName().startsWith("Delete")) {
         let action = createAssociation("Delete Entity Action", command.id, entity.id);
         let mapping = action.createMapping(command.id, entity.id);
