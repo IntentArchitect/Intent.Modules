8be891edc72b6c22f2c2af053ee8bbf7489da114
Gareth Baars <gareth.baars@intentarchitect.com>
2023-11-22 18:32:02 +0100

    downgraded module to fix build.

--- Modules/Intent.Modules.ModuleBuilder/IntentModule.cs
+++ Modules/Intent.Modules.ModuleBuilder/IntentModule.cs
@@ -3,7 +3,7 @@
     public class IntentModule
     {
         public static readonly IntentModule IntentRoslynWeaver = new("Intent.OutputManager.RoslynWeaver", "4.3.1");
-        public static readonly IntentModule IntentCommon = new("Intent.Common", "3.4.4");
+        public static readonly IntentModule IntentCommon = new("Intent.Common", "3.4.3");
         public static readonly IntentModule IntentCommonTypes = new("Intent.Common.Types", "3.3.9");
 
         public IntentModule(string name, string version)
