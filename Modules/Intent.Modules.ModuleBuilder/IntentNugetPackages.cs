840c833a3b8a9019f156584985b66f0ae94d08ad
dandrejvv <dandrejvv@gmail.com>
2023-12-14 20:12:31 +0200

    Ensure Module Builder module uses latest Common module and nuget versions

--- Modules/Intent.Modules.ModuleBuilder/IntentNugetPackages.cs
+++ Modules/Intent.Modules.ModuleBuilder/IntentNugetPackages.cs
@@ -9,7 +9,7 @@ namespace Intent.Modules.ModuleBuilder
         public static NugetPackageInfo IntentPackager = new NugetPackageInfo("Intent.Packager", "3.4.0")
             .SpecifyAssetsBehaviour(new[] { "all" }, new[] { "runtime", "build", "native", "contentfiles", "analyzers", "buildtransitive" });
 
-        public static NugetPackageInfo IntentModulesCommon = new("Intent.Modules.Common", "3.5.0-beta.2");
+        public static NugetPackageInfo IntentModulesCommon = new("Intent.Modules.Common", "3.5.0");
         public static NugetPackageInfo IntentModulesCommonTypes = new("Intent.Modules.Common.Types", "3.3.9");
     }
 }
