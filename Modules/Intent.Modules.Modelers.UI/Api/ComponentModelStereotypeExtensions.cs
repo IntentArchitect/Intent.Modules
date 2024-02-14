5855ea449f1f6e653b52782754c9a20550c1579f
Gareth Baars <gareth.baars@intentarchitect.com>
2024-02-14 08:55:33 +0100

    experimentation continued

---
+++ Modules/Intent.Modules.Modelers.UI/Api/ComponentModelStereotypeExtensions.cs
@@ -0,0 +1,94 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Intent.Metadata.Models;
+using Intent.Modules.Common;
+using Intent.RoslynWeaver.Attributes;
+
+[assembly: DefaultIntentManaged(Mode.Fully)]
+[assembly: IntentTemplate("Intent.ModuleBuilder.Templates.Api.ApiElementModelExtensions", Version = "1.0")]
+
+namespace Intent.Modelers.UI.Api
+{
+    public static class ComponentModelStereotypeExtensions
+    {
+        public static ComponentSettings GetComponentSettings(this ComponentModel model)
+        {
+            var stereotype = model.GetStereotype("93f869a6-f3e1-498e-bf40-6cfe99f012c5");
+            return stereotype != null ? new ComponentSettings(stereotype) : null;
+        }
+
+
+        public static bool HasComponentSettings(this ComponentModel model)
+        {
+            return model.HasStereotype("93f869a6-f3e1-498e-bf40-6cfe99f012c5");
+        }
+
+        public static bool TryGetComponentSettings(this ComponentModel model, out ComponentSettings stereotype)
+        {
+            if (!HasComponentSettings(model))
+            {
+                stereotype = null;
+                return false;
+            }
+
+            stereotype = new ComponentSettings(model.GetStereotype("93f869a6-f3e1-498e-bf40-6cfe99f012c5"));
+            return true;
+        }
+        public static SupportForChildren GetSupportForChildren(this ComponentModel model)
+        {
+            var stereotype = model.GetStereotype("a41c8945-0ca4-4597-b760-66473153b6ab");
+            return stereotype != null ? new SupportForChildren(stereotype) : null;
+        }
+
+
+        public static bool HasSupportForChildren(this ComponentModel model)
+        {
+            return model.HasStereotype("a41c8945-0ca4-4597-b760-66473153b6ab");
+        }
+
+        public static bool TryGetSupportForChildren(this ComponentModel model, out SupportForChildren stereotype)
+        {
+            if (!HasSupportForChildren(model))
+            {
+                stereotype = null;
+                return false;
+            }
+
+            stereotype = new SupportForChildren(model.GetStereotype("a41c8945-0ca4-4597-b760-66473153b6ab"));
+            return true;
+        }
+
+        public class ComponentSettings
+        {
+            private IStereotype _stereotype;
+
+            public ComponentSettings(IStereotype stereotype)
+            {
+                _stereotype = stereotype;
+            }
+
+            public string Name => _stereotype.Name;
+
+            public IIconModel Icon()
+            {
+                return _stereotype.GetProperty<IIconModel>("Icon");
+            }
+
+        }
+
+        public class SupportForChildren
+        {
+            private IStereotype _stereotype;
+
+            public SupportForChildren(IStereotype stereotype)
+            {
+                _stereotype = stereotype;
+            }
+
+            public string Name => _stereotype.Name;
+
+        }
+
+    }
+}
