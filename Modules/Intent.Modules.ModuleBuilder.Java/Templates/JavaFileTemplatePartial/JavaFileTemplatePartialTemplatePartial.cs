33a580c95a01aa812317651af9bea3361a94bb9a
Jonathan Lydall <jonathan.lydall@intentarchitect.com>
2024-02-22 20:57:07 +0200

    Refactor Java modules from needing private NuGet package installed to compile.

--- Modules/Intent.Modules.ModuleBuilder.Java/Templates/JavaFileTemplatePartial/JavaFileTemplatePartialTemplatePartial.cs
+++ Modules/Intent.Modules.ModuleBuilder.Java/Templates/JavaFileTemplatePartial/JavaFileTemplatePartialTemplatePartial.cs
@@ -56,7 +56,10 @@ namespace Intent.Modules.ModuleBuilder.Java.Templates.JavaFileTemplatePartial
             ExecutionContext.EventDispatcher.Publish(new TemplateRegistrationRequiredEvent(this));
             ExecutionContext.EventDispatcher.Publish(new ModuleDependencyRequiredEvent(
                 moduleId: "Intent.Common.Java",
-                moduleVersion: "3.4.3"));
+                moduleVersion: "4.0.0-pre.0"));
+            ExecutionContext.EventDispatcher.Publish(new ModuleDependencyRequiredEvent(
+                moduleId: "Intent.Code.Weaving.Java",
+                moduleVersion: "1.0.0"));
             if (Model.GetModelType() != null)
             {
                 ExecutionContext.EventDispatcher.Publish(new ModuleDependencyRequiredEvent(
