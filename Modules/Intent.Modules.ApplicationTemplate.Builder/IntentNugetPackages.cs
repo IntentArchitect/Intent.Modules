b945ecaa22c6bd8eefb6b45de43e65e01bfe9309
Gareth Baars <gareth.baars@intentarchitect.com>
2024-03-04 09:03:02 +0100

    update Packager version for MB

--- Modules/Intent.Modules.ApplicationTemplate.Builder/IntentNugetPackages.cs
+++ Modules/Intent.Modules.ApplicationTemplate.Builder/IntentNugetPackages.cs
@@ -4,7 +4,7 @@ namespace Intent.Modules.ApplicationTemplate.Builder
 {
     public static class IntentNugetPackages
     {
-        public static NugetPackageInfo IntentPackager = new NugetPackageInfo("Intent.Packager", "3.4.0")
+        public static NugetPackageInfo IntentPackager = new NugetPackageInfo("Intent.Packager", "3.4.2")
             .SpecifyAssetsBehaviour(new[] { "all" }, new[] { "runtime", "build", "native", "contentfiles", "analyzers", "buildtransitive" });
     }
 }
