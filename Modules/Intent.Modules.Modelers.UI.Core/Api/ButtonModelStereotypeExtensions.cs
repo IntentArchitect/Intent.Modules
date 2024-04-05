7ecfb8d412887206e99f95f92ee382e8d74c92cb
Gareth Baars <gareth.baars@intentarchitect.com>
2024-04-05 17:30:07 +0200

    UI related wip

--- Modules/Intent.Modules.Modelers.UI.Core/Api/ButtonModelStereotypeExtensions.cs
+++ Modules/Intent.Modules.Modelers.UI.Core/Api/ButtonModelStereotypeExtensions.cs
@@ -47,9 +47,52 @@ namespace Intent.Modelers.UI.Core.Api
 
             public string Name => _stereotype.Name;
 
+            public TypeOptions Type()
+            {
+                return new TypeOptions(_stereotype.GetProperty<string>("Type"));
+            }
+
             public string OnClick()
             {
                 return _stereotype.GetProperty<string>("On Click");
+            }
+
+            public class TypeOptions
+            {
+                public readonly string Value;
+
+                public TypeOptions(string value)
+                {
+                    Value = value;
+                }
+
+                public TypeOptionsEnum AsEnum()
+                {
+                    switch (Value)
+                    {
+                        case "Default":
+                            return TypeOptionsEnum.Default;
+                        case "Submit":
+                            return TypeOptionsEnum.Submit;
+                        default:
+                            throw new ArgumentOutOfRangeException();
+                    }
+                }
+
+                public bool IsDefault()
+                {
+                    return Value == "Default";
+                }
+                public bool IsSubmit()
+                {
+                    return Value == "Submit";
+                }
+            }
+
+            public enum TypeOptionsEnum
+            {
+                Default,
+                Submit
             }
 
         }
