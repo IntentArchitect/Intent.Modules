55715102167d4fa74221a4f37eb71f8cff142f69
Dandré <dandrejvv@users.noreply.github.com>
2024-04-09 12:56:48 +0200

    Migration class more generic

--- Modules/Intent.Modules.Common.CSharp/Migrations/Migration_03_06_00_Pre_08.cs
+++ Modules/Intent.Modules.Common.CSharp/Migrations/Migration_03_06_00_Pre_08.cs
@@ -9,34 +9,26 @@ using Intent.Utils;
 
 namespace Intent.Modules.Common.CSharp.Migrations;
 
-public class Migration_06_03_00_Pre_06 : IModuleMigration
+public class Migration_03_06_00_Pre_08 : IModuleMigration
 {
     private readonly IApplicationConfigurationProvider _configurationProvider;
-    private const string DomainDesignerId = "6ab29b31-27af-4f56-a67c-986d82097d63";
-    private const string ServicesDesignerId = "81104ae6-2bc5-4bae-b05a-f987b0372d81";
-    private const string EventingDesignerId = "822e4254-9ced-4dd1-ad56-500b861f7e4d";
     private const string DateTypeId = "1fbaa056-b666-4f25-b8fd-76fe3165acc8";
     private const string DateTimeTypeId = "a4107c29-7851-4121-9416-cf1236908f1e";
     private const string MetadataKey = "common-charp-migration-date-to-datetime";
     
-    public Migration_06_03_00_Pre_06(IApplicationConfigurationProvider configurationProvider)
+    public Migration_03_06_00_Pre_08(IApplicationConfigurationProvider configurationProvider)
     {
         _configurationProvider = configurationProvider;
     }
 
     public string ModuleId => "Intent.Common.CSharp";
-    public string ModuleVersion => "3.6.0-pre.6";
+    public string ModuleVersion => "3.6.0-pre.8";
     
     public void Up()
     {
         var app = ApplicationPersistable.Load(_configurationProvider.GetApplicationConfig().FilePath);
-        var domainDesigner = app.GetDesigner(DomainDesignerId);
-        var servicesDesigner = app.GetDesigner(ServicesDesignerId);
-        var eventingDesigner = app.GetDesigner(EventingDesignerId);
 
-        var packages = (domainDesigner?.GetPackages() ?? Enumerable.Empty<PackageModelPersistable>())
-            .Concat(servicesDesigner?.GetPackages() ?? Enumerable.Empty<PackageModelPersistable>())
-            .Concat(eventingDesigner?.GetPackages() ?? Enumerable.Empty<PackageModelPersistable>());
+        var packages = app.GetDesigners().SelectMany(x => x.GetPackages());
 
         foreach (var package in packages)
         {
@@ -59,13 +51,8 @@ public class Migration_06_03_00_Pre_06 : IModuleMigration
     public void Down()
     {
         var app = ApplicationPersistable.Load(_configurationProvider.GetApplicationConfig().FilePath);
-        var domainDesigner = app.GetDesigner(DomainDesignerId);
-        var servicesDesigner = app.GetDesigner(ServicesDesignerId);
-        var eventingDesigner = app.GetDesigner(EventingDesignerId);
-
-        var packages = (domainDesigner?.GetPackages() ?? Enumerable.Empty<PackageModelPersistable>())
-            .Concat(servicesDesigner?.GetPackages() ?? Enumerable.Empty<PackageModelPersistable>())
-            .Concat(eventingDesigner?.GetPackages() ?? Enumerable.Empty<PackageModelPersistable>());
+        
+        var packages = app.GetDesigners().SelectMany(x => x.GetPackages());
 
         foreach (var package in packages)
         {
