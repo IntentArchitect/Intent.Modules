82d572f2bc4e5d95dfe4eb04859cada27cd4c145
Dandré <dandrejvv@users.noreply.github.com>
2024-07-09 08:37:06 +0200

    ModuleBuilder - Bumped core nuget and module versions to fix compilation errors.

--- Modules/Intent.Modules.ModuleBuilder/IntentModule.cs
+++ Modules/Intent.Modules.ModuleBuilder/IntentModule.cs
@@ -3,8 +3,8 @@
     public class IntentModule
     {
         public static readonly IntentModule IntentRoslynWeaver = new("Intent.OutputManager.RoslynWeaver", "4.5.1");
-        public static readonly IntentModule IntentCommon = new("Intent.Common", "3.5.0");
-        public static readonly IntentModule IntentCommonTypes = new("Intent.Common.Types", "3.3.9");
+        public static readonly IntentModule IntentCommon = new("Intent.Common", "3.6.0");
+        public static readonly IntentModule IntentCommonTypes = new("Intent.Common.Types", "3.4.0");
 
         public IntentModule(string name, string version)
         {
