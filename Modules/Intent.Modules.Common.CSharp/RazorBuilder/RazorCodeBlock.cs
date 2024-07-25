ca4574ec357ed269fc310c7276862dbc43f10375
Gareth Baars <gareth.baars@intentarchitect.com>
2024-07-25 09:59:35 +0200

    Removed unused code from RazorCodeBlock.cs

--- Modules/Intent.Modules.Common.CSharp/RazorBuilder/RazorCodeBlock.cs
+++ Modules/Intent.Modules.Common.CSharp/RazorBuilder/RazorCodeBlock.cs
@@ -17,76 +17,19 @@ internal class RazorCodeBlock : CSharpClass, IRazorCodeBlock
     }
 
     public IRazorFile RazorFile { get; }
-
-    //public IBuildsCSharpMembers InsertField(int index, string type, string name, Action<ICSharpField> configure = null)
-    //{
-    //    var field = new CSharpField(type, name, this)
-    //    {
-    //        BeforeSeparator = CSharpCodeSeparatorType.NewLine,
-    //        AfterSeparator = CSharpCodeSeparatorType.NewLine
-    //    };
-    //    Declarations.Insert(index, field);
-    //    configure?.Invoke(field);
-    //    return this;
-    //}
-
-    //public IBuildsCSharpMembers AddField(string type, string name, Action<ICSharpField> configure = null)
-    //{
-    //    return InsertField(Declarations.Count, type, name, configure);
-    //}
-
-    //public IBuildsCSharpMembers InsertProperty(int index, string type, string name, Action<ICSharpProperty> configure = null)
-    //{
-    //    var property = new CSharpProperty(type, name, this)
-    //    {
-    //        BeforeSeparator = CSharpCodeSeparatorType.EmptyLines,
-    //        AfterSeparator = CSharpCodeSeparatorType.EmptyLines
-    //    };
-    //    Declarations.Insert(index, property);
-    //    configure?.Invoke(property);
-    //    return this;
-    //}
-
-    //public IBuildsCSharpMembers AddProperty(string type, string name, Action<ICSharpProperty> configure = null)
-    //{
-    //    return InsertProperty(Declarations.Count, type, name, configure);
-    //}
-
-    //public IBuildsCSharpMembers InsertMethod(int index, string type, string name, Action<ICSharpClassMethod> configure = null)
-    //{
-    //    var method = new CSharpClassMethod(type, name, this)
-    //    {
-    //        BeforeSeparator = CSharpCodeSeparatorType.EmptyLines,
-    //        AfterSeparator = CSharpCodeSeparatorType.EmptyLines
-    //    };
-    //    Declarations.Insert(index, method);
-    //    configure?.Invoke(method);
-    //    return this;
-    //}
-
-    //public IBuildsCSharpMembers AddMethod(string type, string name, Action<ICSharpClassMethod> configure = null)
-    //{
-    //    return InsertMethod(Declarations.Count, type, name, configure);
-    //}
-
-    //public IBuildsCSharpMembers AddClass(string name, Action<ICSharpClass> configure = null)
-    //{
-    //    var @class = new CSharpClass(name, File)
-    //    {
-    //        BeforeSeparator = CSharpCodeSeparatorType.EmptyLines,
-    //        AfterSeparator = CSharpCodeSeparatorType.EmptyLines
-    //    };
-    //    Declarations.Add(@class);
-    //    configure?.Invoke(@class);
-    //    return this;
-    //}
-
+    public IList<IRazorFileNode> ChildNodes { get; } = new List<IRazorFileNode>();
+    public IRazorFileNode? Parent { get; set; }
     ICSharpTemplate IBuildsCSharpMembers.Template => File.Template;
 
-    //public int IndexOf(ICodeBlock codeBlock)
-    //{
-    //    return Declarations.IndexOf(codeBlock);
-    //}
+    public void AddChildNode(IRazorFileNode node)
+    {
+        throw new InvalidOperationException($"Cannot add child razor nodes to a {nameof(RazorCodeBlock)}");
+    }
+
+    public void InsertChildNode(int index, IRazorFileNode node)
+    {
+        throw new InvalidOperationException($"Cannot add child razor nodes to a {nameof(RazorCodeBlock)}");
+    }
 
     public override string GetText(string indentation)
     {
@@ -96,17 +39,4 @@ internal class RazorCodeBlock : CSharpClass, IRazorCodeBlock
 {indentation}}}
 ";
     }
-
-    public void AddChildNode(IRazorFileNode node)
-    {
-        throw new NotImplementedException();
-    }
-
-    public void InsertChildNode(int index, IRazorFileNode node)
-    {
-        throw new NotImplementedException();
-    }
-
-    public IList<IRazorFileNode> ChildNodes { get; }
-    public IRazorFileNode? Parent { get; set; }
 }
