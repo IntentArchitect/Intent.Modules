cad4981419fa7a93515de639df17d99c651c80bb
Dandré <dandrejvv@users.noreply.github.com>
2024-08-22 17:48:20 +0200

    Added documentation for TemplateFileConfig.

--- Modules/Intent.Modules.Common/Templates/TemplateFileConfig.cs
+++ Modules/Intent.Modules.Common/Templates/TemplateFileConfig.cs
@@ -5,6 +5,25 @@ namespace Intent.Modules.Common.Templates
 {
     public class TemplateFileConfig : ITemplateFileConfig
     {
+        /// <summary>
+        /// Template output configuration.
+        /// </summary>
+        /// <param name="fileName">
+        /// The name of the file to be generated.
+        /// This can also be used to define "dotfiles" such as ".dockerignore".
+        /// </param>
+        /// <param name="fileExtension">
+        /// The extension of the file, such as ".cs" or ".json". If you want to define a "dotfile" (like ".dockerignore") you will need to define it in <see cref="fileName"/>.
+        /// </param>
+        /// <param name="relativeLocation">
+        /// The relative location to where the template is installed where the output should be generated. Defaults to an empty string.
+        /// </param>
+        /// <param name="overwriteBehaviour">
+        /// Indicates the behaviour for overwriting an existing file. The default is <see cref="OverwriteBehaviour.Always"/>.
+        /// </param>
+        /// <param name="codeGenType">
+        /// Specifies the type of code generation. The default is <see cref="Common.CodeGenType.Basic"/>.
+        /// </param>
         public TemplateFileConfig(
             string fileName,
             string fileExtension,
