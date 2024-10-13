a51e9f6feabc31917f2d5fb490b796853e7c6af6
Joel Turner <joel.turner@intentarchitect.com>
2024-10-13 22:33:04 +0200

    Added Framework Reference and a cleaner "Project reference"

---
+++ Modules/Intent.Modules.Common.CSharp/VisualStudio/FrameworkReference.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intent.Modules.Common.VisualStudio
+{
+    public class FrameworkReference : IAssemblyReference
+    {
+        public FrameworkReference(string frameworkName)
+        {
+            Library = frameworkName;
+        }
+
+        public bool Equals(IAssemblyReference other)
+        {
+            if (other == null)
+                return false;
+            return Library == other.Library;
+        }
+
+        public string Library { get; }
+        public string HintPath { get; }
+        public bool HasHintPath()
+        {
+            return false;
+        }
+    }
+}
---
+++ Modules/Intent.Modules.Common.CSharp/VisualStudio/ProjectReference.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intent.Modules.Common.VisualStudio
+{
+    public class ProjectReference : IAssemblyReference
+    {
+        public string Library { get; }
+        public string HintPath { get; }
+
+        public ProjectReference(string projectPath, string hintPath = null)
+        {
+            Library = projectPath;
+            HintPath = hintPath;
+        }
+
+        public bool HasHintPath()
+        {
+            return true;
+        }
+
+        public bool Equals(IAssemblyReference other)
+        {
+            if (other == null)
+                return false;
+            return Library == other.Library;
+        }
+    }
+}
