8029c4804f7416a70702edc1065cdd493fbc8cc1
Joel Turner <joel.turner@intentarchitect.com>
2024-11-01 12:17:48 +0200

    Added Event for Separate database multitenancy Added 2.1 Framework to for NuGet packages

---
+++ Modules/Intent.Modules.Common.CSharp/Multitenancy/MultitenantConnectionStringRegistrationRequest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intent.Modules.Common.CSharp.Multitenancy
+{
+    public class MultitenantConnectionStringRegistrationRequest
+    {
+        public MultitenantConnectionStringRegistrationRequest(string name, string connectionStringTemplate)
+        {
+            Name = name;
+            ConnectionStringTemplate = connectionStringTemplate;
+        }
+
+        public string Name { get; }
+        public string ConnectionStringTemplate { get; }
+    }
+}
