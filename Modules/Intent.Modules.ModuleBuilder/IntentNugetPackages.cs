96776c9aedef108188c900c6d5b49d6eda465307
Jonathan Lydall <jonathan.lydall@intentarchitect.com>
2024-11-28 16:46:46 +0200

    Try fix package downgrade error.

--- Modules/Intent.Modules.ModuleBuilder/IntentNugetPackages.cs
+++ Modules/Intent.Modules.ModuleBuilder/IntentNugetPackages.cs
@@ -9,7 +9,7 @@ namespace Intent.Modules.ModuleBuilder
         public static NugetPackageInfo IntentPackager = new NugetPackageInfo("Intent.Packager", "3.4.3")
             .SpecifyAssetsBehaviour(new[] { "all" }, new[] { "runtime", "build", "native", "contentfiles", "analyzers", "buildtransitive" });
 
-        public static NugetPackageInfo IntentModulesCommon = new("Intent.Modules.Common", "3.7.2-pre.0");
+        public static NugetPackageInfo IntentModulesCommon = new("Intent.Modules.Common", "3.7.2");
         public static NugetPackageInfo IntentModulesCommonTypes = new("Intent.Modules.Common.Types", "3.4.0");
     }
 }
