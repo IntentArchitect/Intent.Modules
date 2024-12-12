cd8fc9721ffc734145a4ed664fa0fb84b81fadfc
Jonathan Lydall <jonathan.lydall@intentarchitect.com>
2024-12-12 16:13:19 +0200

    Add warning.

--- Modules/Intent.Modules.Modelers.Eventing/ApiMetadataDesignerExtensions.cs
+++ Modules/Intent.Modules.Modelers.Eventing/ApiMetadataDesignerExtensions.cs
@@ -1,14 +1,15 @@
+using System;
+using System.Linq;
 using Intent.Engine;
 using Intent.Metadata.Models;
-using Intent.RoslynWeaver.Attributes;
-
-[assembly: DefaultIntentManaged(Mode.Fully)]
-[assembly: IntentTemplate("Intent.ModuleBuilder.Templates.Api.ApiMetadataDesignerExtensions", Version = "1.0")]
+using Intent.Modelers.Services.Api;
 
 namespace Intent.Modelers.Eventing.Api
 {
     public static class ApiMetadataDesignerExtensions
     {
+        private static bool _applicationElementCheckPerformed;
+
         public const string EventingDesignerId = "822e4254-9ced-4dd1-ad56-500b861f7e4d";
         public static IDesigner Eventing(this IMetadataManager metadataManager, IApplication application)
         {
@@ -17,6 +18,21 @@ namespace Intent.Modelers.Eventing.Api
 
         public static IDesigner Eventing(this IMetadataManager metadataManager, string applicationId)
         {
+            if (!_applicationElementCheckPerformed)
+            {
+                var applicationElement = metadataManager.Services(applicationId).GetApplicationModels().FirstOrDefault()?.InternalElement;
+                if (applicationElement != null)
+                {
+                    throw new Exception(
+                        "\"Application\" element type detected in Services Designer, you will need to migrate from the " +
+                        "removed Eventing designer modelling paradigm. Refer to " +
+                        "https://docs.intentarchitect.com/articles/application-development/modelling/services-designer/message-based-integration-modeling/message-based-integration-modeling.html#migrating-from-the-eventing-designer " +
+                        "for more information.");
+                }
+
+                _applicationElementCheckPerformed = true;
+            }
+
             return metadataManager.GetDesigner(applicationId, EventingDesignerId);
         }
 
