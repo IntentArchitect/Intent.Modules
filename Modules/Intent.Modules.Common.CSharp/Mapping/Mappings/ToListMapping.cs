3d8732793017e068adacc01b40579fd47c60724e
JP Seini <jp.seini@intentarchitect.com>
2025-02-28 11:25:52 +0200

    Added `ToListMapping` to cater for `Enum Collection` to `Enum Collection` mapping (but can be used more generally if required)

---
+++ Modules/Intent.Modules.Common.CSharp/Mapping/Mappings/ToListMapping.cs
@@ -0,0 +1,36 @@
+﻿using Intent.Metadata.Models;
+using Intent.Modules.Common.CSharp.Builder;
+using Intent.Modules.Common.CSharp.Templates;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intent.Modules.Common.CSharp.Mapping;
+
+public class ToListMapping : CSharpMappingBase
+{
+    private readonly ICSharpFileBuilderTemplate _template;
+
+    public ToListMapping(ICanBeReferencedType model, IElementToElementMappedEnd mapping, IList<MappingModel> children, ICSharpFileBuilderTemplate template) : base(model, mapping, children, template)
+    {
+        _template = template;
+    }
+
+    public ToListMapping(MappingModel model, ICSharpFileBuilderTemplate template) : base(model, template)
+    {
+        _template = template;
+    }
+
+    public override CSharpStatement GetSourceStatement(bool? targetIsNullable = null)
+    {
+        if((Mapping.SourceElement?.TypeReference?.IsCollection ?? false) != true && (Mapping.TargetElement?.TypeReference?.IsCollection ?? false) != true)
+        {
+            return base.GetSourceStatement(targetIsNullable);
+        }
+
+        Template.AddUsing("System.Linq");
+        return new CSharpAccessMemberStatement(base.GetSourceStatement(), new CSharpInvocationStatement("ToList").WithoutSemicolon());
+    }
+}
--- Modules/Intent.Modules.Common.CSharp/release-notes.md
+++ Modules/Intent.Modules.Common.CSharp/release-notes.md
@@ -1,6 +1,7 @@
 ﻿### Version 3.8.10
 
 - Improvement: Introduced `ExposeProgramClass()` to `IAppStartupFile` to allow for referencing the Program class under certain circumstances such as in WebApplicationFactories.
+- Improvement: Added `ToListMapping` to cater for `Enum Collection` to `Enum Collection` mapping (but can be used more generally if required)
 
 ### Version 3.8.9
 
