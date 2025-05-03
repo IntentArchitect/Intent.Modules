484b021f7ef3117e7dc723940a7e75cff9e5b208
Gareth Baars <gareth.baars@intentarchitect.com>
2025-05-03 20:10:29 +0200

    Removing deprecated "Send Command" and "Call Service Operation" action - migrated to generic "Service Invocation" action (from Modelers.Services)

---
+++ Modules/Intent.Modules.Modelers.Services.EventInteractions/Api/SendCommandModel.cs
@@ -0,0 +1,201 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Intent.Metadata.Models;
+using Intent.Modules.Common;
+using Intent.RoslynWeaver.Attributes;
+
+[assembly: DefaultIntentManaged(Mode.Fully)]
+[assembly: IntentTemplate("Intent.ModuleBuilder.Templates.Api.ApiAssociationModel", Version = "1.0")]
+
+namespace Intent.Modelers.Services.EventInteractions
+{
+    [IntentManaged(Mode.Fully, Signature = Mode.Fully)]
+    public class SendCommandModel : IMetadataModel
+    {
+        public const string SpecializationType = "Send Command";
+        public const string SpecializationTypeId = "38a3de5a-ca88-4f6e-88b9-88e5953936b2";
+        protected readonly IAssociation _association;
+        protected SendCommandSourceEndModel _sourceEnd;
+        protected SendCommandTargetEndModel _targetEnd;
+
+        public SendCommandModel(IAssociation association, string requiredType = SpecializationType)
+        {
+            if (!requiredType.Equals(association.SpecializationType, StringComparison.InvariantCultureIgnoreCase))
+            {
+                throw new Exception($"Cannot create a '{GetType().Name}' from association with specialization type '{association.SpecializationType}'. Must be of type '{SpecializationType}'");
+            }
+            _association = association;
+        }
+
+        public static SendCommandModel CreateFromEnd(IAssociationEnd associationEnd)
+        {
+            var association = new SendCommandModel(associationEnd.Association);
+            return association;
+        }
+
+        public string Id => _association.Id;
+
+        public SendCommandSourceEndModel SourceEnd => _sourceEnd ??= new SendCommandSourceEndModel(_association.SourceEnd, this);
+
+        public SendCommandTargetEndModel TargetEnd => _targetEnd ??= new SendCommandTargetEndModel(_association.TargetEnd, this);
+
+        public IAssociation InternalAssociation => _association;
+
+        public override string ToString()
+        {
+            return _association.ToString();
+        }
+
+        public bool Equals(SendCommandModel other)
+        {
+            return Equals(_association, other?._association);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((SendCommandModel)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return (_association != null ? _association.GetHashCode() : 0);
+        }
+    }
+
+    [IntentManaged(Mode.Fully)]
+    public class SendCommandSourceEndModel : SendCommandEndModel
+    {
+        public const string SpecializationTypeId = "6b9e34dd-b50f-4998-a3cf-93dde7b2d51e";
+        public const string SpecializationType = "Send Command Source End";
+
+        public SendCommandSourceEndModel(IAssociationEnd associationEnd, SendCommandModel association) : base(associationEnd, association)
+        {
+        }
+    }
+
+    [IntentManaged(Mode.Fully)]
+    public class SendCommandTargetEndModel : SendCommandEndModel
+    {
+        public const string SpecializationTypeId = "d3096261-1268-440f-8db3-0a6b8b4786cc";
+        public const string SpecializationType = "Send Command Target End";
+
+        public SendCommandTargetEndModel(IAssociationEnd associationEnd, SendCommandModel association) : base(associationEnd, association)
+        {
+        }
+
+        public IEnumerable<IElementToElementMapping> Mappings => _associationEnd.Mappings;
+    }
+
+    [IntentManaged(Mode.Fully)]
+    public class SendCommandEndModel : ITypeReference, IMetadataModel, IHasName, IHasStereotypes, IElementWrapper
+    {
+        protected readonly IAssociationEnd _associationEnd;
+        private readonly SendCommandModel _association;
+
+        public SendCommandEndModel(IAssociationEnd associationEnd, SendCommandModel association)
+        {
+            _associationEnd = associationEnd;
+            _association = association;
+        }
+
+        public static SendCommandEndModel Create(IAssociationEnd associationEnd)
+        {
+            var association = new SendCommandModel(associationEnd.Association);
+            return association.TargetEnd.Id == associationEnd.Id ? (SendCommandEndModel)association.TargetEnd : association.SourceEnd;
+        }
+
+        public string Id => _associationEnd.Id;
+        public string SpecializationType => _associationEnd.SpecializationType;
+        public string SpecializationTypeId => _associationEnd.SpecializationTypeId;
+        public string Name => _associationEnd.Name;
+        public SendCommandModel Association => _association;
+        public IElement InternalElement => _associationEnd;
+        public IAssociationEnd InternalAssociationEnd => _associationEnd;
+        public IAssociation InternalAssociation => _association.InternalAssociation;
+        public bool IsNavigable => _associationEnd.IsNavigable;
+        public bool IsNullable => _associationEnd.TypeReference.IsNullable;
+        public bool IsCollection => _associationEnd.TypeReference.IsCollection;
+        public ICanBeReferencedType Element => _associationEnd.TypeReference.Element;
+        public IEnumerable<ITypeReference> GenericTypeParameters => _associationEnd.TypeReference.GenericTypeParameters;
+        public ITypeReference TypeReference => this;
+        public IPackage Package => Element?.Package;
+        public string Comment => _associationEnd.Comment;
+        public IEnumerable<IStereotype> Stereotypes => _associationEnd.Stereotypes;
+
+        public SendCommandEndModel OtherEnd()
+        {
+            return this.Equals(_association.SourceEnd) ? (SendCommandEndModel)_association.TargetEnd : (SendCommandEndModel)_association.SourceEnd;
+        }
+
+        public bool IsTargetEnd()
+        {
+            return _associationEnd.IsTargetEnd();
+        }
+
+        public bool IsSourceEnd()
+        {
+            return _associationEnd.IsSourceEnd();
+        }
+
+        public override string ToString()
+        {
+            return _associationEnd.ToString();
+        }
+
+        public bool Equals(SendCommandEndModel other)
+        {
+            return Equals(_associationEnd, other._associationEnd);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((SendCommandEndModel)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return (_associationEnd != null ? _associationEnd.GetHashCode() : 0);
+        }
+    }
+
+    [IntentManaged(Mode.Fully)]
+    public static class SendCommandEndModelExtensions
+    {
+        public static bool IsSendCommandEndModel(this ICanBeReferencedType type)
+        {
+            return IsSendCommandTargetEndModel(type) || IsSendCommandSourceEndModel(type);
+        }
+
+        public static SendCommandEndModel AsSendCommandEndModel(this ICanBeReferencedType type)
+        {
+            return (SendCommandEndModel)type.AsSendCommandTargetEndModel() ?? (SendCommandEndModel)type.AsSendCommandSourceEndModel();
+        }
+
+        public static bool IsSendCommandTargetEndModel(this ICanBeReferencedType type)
+        {
+            return type != null && type is IAssociationEnd associationEnd && associationEnd.SpecializationTypeId == SendCommandTargetEndModel.SpecializationTypeId;
+        }
+
+        public static SendCommandTargetEndModel AsSendCommandTargetEndModel(this ICanBeReferencedType type)
+        {
+            return type.IsSendCommandTargetEndModel() ? new SendCommandModel(((IAssociationEnd)type).Association).TargetEnd : null;
+        }
+
+        public static bool IsSendCommandSourceEndModel(this ICanBeReferencedType type)
+        {
+            return type != null && type is IAssociationEnd associationEnd && associationEnd.SpecializationTypeId == SendCommandSourceEndModel.SpecializationTypeId;
+        }
+
+        public static SendCommandSourceEndModel AsSendCommandSourceEndModel(this ICanBeReferencedType type)
+        {
+            return type.IsSendCommandSourceEndModel() ? new SendCommandModel(((IAssociationEnd)type).Association).SourceEnd : null;
+        }
+    }
+}
---
+++ Modules/Intent.Modules.Modelers.Services.EventInteractions/Migrations/Migration_2_0_0_pre_0.cs
@@ -0,0 +1,69 @@
+using System.Linq;
+using Intent.Engine;
+using Intent.Persistence;
+using Intent.Plugins;
+using Intent.RoslynWeaver.Attributes;
+
+[assembly: DefaultIntentManaged(Mode.Merge)]
+[assembly: IntentTemplate("Intent.ModuleBuilder.Templates.Migrations.OnVersionMigration", Version = "1.0")]
+
+namespace Intent.Modules.Modelers.Services.EventInteractions.Migrations
+{
+    public class Migration_2_0_0_pre_0 : IModuleMigration
+    {
+        private readonly IPersistenceLoader _persistenceLoader;
+        private readonly IApplicationConfigurationProvider _applicationConfigurationProvider;
+
+        public Migration_2_0_0_pre_0(IPersistenceLoader persistenceLoader, IApplicationConfigurationProvider applicationConfigurationProvider)
+        {
+            _persistenceLoader = persistenceLoader;
+            _applicationConfigurationProvider = applicationConfigurationProvider;
+        }
+
+        public string ModuleId { get; } = "Intent.Modelers.Services.EventInteractions";
+        public string ModuleVersion { get; } = "2.0.0-pre.0";
+        public const string ServicesDesignerId = "81104ae6-2bc5-4bae-b05a-f987b0372d81";
+
+        public void Up()
+        {
+            var application = _persistenceLoader.LoadApplication(_applicationConfigurationProvider.GetApplicationConfig().FilePath);
+            var designer = application.GetDesigner(ServicesDesignerId);
+            var packages = designer.GetPackages();
+
+            foreach (var package in packages)
+            {
+                var deprecatedSendCommandAssociations = package.Associations.Where(x =>
+                    x.AssociationTypeId == "38a3de5a-ca88-4f6e-88b9-88e5953936b2" // Send Command
+                    || x.AssociationTypeId == "9510ff76-fba3-4eca-a5dd-0cfefc8f5bb6" // Call Service Operation
+                    );
+                if (!deprecatedSendCommandAssociations.Any())
+                {
+                    continue;
+                }
+
+                foreach (var association in deprecatedSendCommandAssociations)
+                {
+                    association.AssociationType = "Service Invocation";
+                    association.AssociationTypeId = "3e69085c-fa2f-44bd-93eb-41075fd472f8";
+                    association.SourceEnd.SpecializationType = "Service Invocation Source End";
+                    association.SourceEnd.SpecializationTypeId = "ee56bd48-8eff-4fff-8d3a-87731d002335";
+                    association.TargetEnd.SpecializationType = "Service Invocation Target End";
+                    association.TargetEnd.SpecializationTypeId = "093e5909-ffe4-4510-b3ea-532f30212f3c";
+                    foreach (var mapping in association.TargetEnd.Mappings.Where(x =>
+                                     x.TypeId == "a4c4c5cc-76df-48ed-9d4e-c35caf44b567" // Integration Event Handler Mapping
+                             ))
+                    {
+                        mapping.Type = "Invocation Mapping";
+                        mapping.TypeId = "a4c4c5cc-76df-48ed-9d4e-c35caf44b567";
+                    }
+                }
+
+                package.Save();
+            }
+        }
+
+        public void Down()
+        {
+        }
+    }
+}
