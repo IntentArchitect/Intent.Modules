5334328d41cdee9525bfd1e04eb7bbdb8a5d3fea
Joel Turner <joel.turner@intentarchitect.com>
2025-06-27 13:31:18 +0200

    Upgraded Expose Service to use the actual Operation Script

--- DesignerMacros/src/services-expose-as-http-endpoint/operation/common.ts
+++ DesignerMacros/src/services-expose-as-http-endpoint/operation/common.ts
@@ -1,27 +1,8 @@
-/// <reference path="../operation/common.ts" />
+/// <reference path="../../../typings/elementmacro.context.api.d.ts" />
+/// <reference path="../_common/getDefaultRoutePrefix.ts" />
+/// <reference path="../_common/getRouteParts.ts" />
+/// <reference path="../../common/getMappedDomainElement.ts" />
 
-function exposeAsHttpEndPoint(element: MacroApi.Context.IElementApi): void {
-    const httpServiceSettingsId = "c29224ec-d473-4b95-ad4a-ec55c676c4fd"; // from WebApi module
-
-    if (!element.hasStereotype(httpServiceSettingsId)) {
-        element.addStereotype(httpServiceSettingsId);
-
-        let serviceBaseName = removeSuffix(element.getName(), "Service");
-        element.getStereotype(httpServiceSettingsId).getProperty("Route").setValue(getRoute(serviceBaseName));
-    }
-
-    let parentRoute = element.getStereotype(httpServiceSettingsId).getProperty("Route").getValue().toString();
-
-    element.getChildren("Operation").forEach(x => {
-        applyHttpSettingsToOperations(x, parentRoute);
-    })
-}
-
-function getRoute(serviceBaseName: string): string {
-    return `${getDefaultRoutePrefix(true)}${toKebabCase(serviceBaseName)}`;
-}
-
-/*
 function applyHttpSettingsToOperations(operation: MacroApi.Context.IElementApi, existingRoute: string = ``): void {
     const httpSettingsId = "b4581ed2-42ec-4ae2-83dd-dcdd5f0837b6"; // from WebApi module
     const parameterSettingsId = "d01df110-1208-4af8-a913-92a49d219552"; // from WebApi module
@@ -37,6 +18,8 @@ function applyHttpSettingsToOperations(operation: MacroApi.Context.IElementApi, 
         var serviceName = operation.getParent().getName();
         serviceDomain = singularize(serviceName.replace(`Service`, ``))
     }
+    
+    const domainElement = getMappedDomainElement(operation);
 
     // filter out some common phrases
     let toReplace = [
@@ -64,25 +47,37 @@ function applyHttpSettingsToOperations(operation: MacroApi.Context.IElementApi, 
         operationName = operationName.replace(search, '');
     });
 
+    let routePrefix:string = "";
+    if (domainElement != null && domainElement.entityDomainElementDetails.hasOwningEntity()){
+        let routes = getOwningAggregateRouting(operation, domainElement );
+        routePrefix = routes.join("/");
+        serviceDomain = singularize( domainElement.entityDomainElementDetails.entity.getName());
+    }
+
+    let entity = domainElement?.entityDomainElementDetails?.entity;
+
     // first check if its the standard default operations
     // if its not one of the "defaults" setup by the CRUD accelerator
     // then calculate the route
     const httpSettings = operation.getStereotype(httpSettingsId);
     if(operation.getName() === `Create${serviceDomain}`) {
         httpSettings.getProperty("Verb").setValue("POST");
-        httpSettings.getProperty("Route").setValue(``)
+        httpSettings.getProperty("Route").setValue(getRouteInfo(operation, routePrefix, false, entity))
     } else if (operation.getName() === `Update${serviceDomain}`) {
         httpSettings.getProperty("Verb").setValue("PUT");
-        httpSettings.getProperty("Route").setValue(`${(operation.getChildren().some(x => x.getName().toLowerCase() == "id") ? `{id}` : "")}`)
+        httpSettings.getProperty("Route").setValue(getRouteInfo(operation, routePrefix, true, entity))
     } else if (operation.getName() === `Delete${serviceDomain}`) {
         httpSettings.getProperty("Verb").setValue("DELETE");
-        httpSettings.getProperty("Route").setValue(`${(operation.getChildren().some(x => x.getName().toLowerCase() == "id") ? `{id}` : "")}`)
+        httpSettings.getProperty("Route").setValue(getRouteInfo(operation, routePrefix, true, entity))
     } else if (operation.getName() === `Find${serviceDomain}ById`) {
         httpSettings.getProperty("Verb").setValue("GET");
-        httpSettings.getProperty("Route").setValue(`${(operation.getChildren().some(x => x.getName().toLowerCase() == "id") ? `{id}` : "")}`)
+        httpSettings.getProperty("Route").setValue(getRouteInfo(operation, routePrefix, true, entity))
     }else if (operation.getName() === `Find${pluralize(serviceDomain)}`) {
         httpSettings.getProperty("Verb").setValue("GET");
-        httpSettings.getProperty("Route").setValue(`${(operation.getChildren().some(x => x.getName().toLowerCase() == "id") ? `{id}` : "")}`)
+        httpSettings.getProperty("Route").setValue(getRouteInfo(operation, routePrefix, false, entity))
+    } else if (isMappedDomainOperation(operation)) {
+        httpSettings.getProperty("Verb").setValue("PUT");
+        httpSettings.getProperty("Route").setValue(getRouteInfo(operation, routePrefix, true, entity, kebabCaseAcronymCorrection(toKebabCase(operationName), operationName) ))
     } else if (operation.getName().startsWith("Get") || operation.getName().startsWith("Find") || operation.getName().startsWith("Lookup")) {
         httpSettings.getProperty("Verb").setValue("GET");
         httpSettings.getProperty("Route").setValue(`${kebabCaseAcronymCorrection(toKebabCase(operationName), operationName)}${(operation.getChildren().some(x => x.getName().toLowerCase() == "id") ? `/{id}` : "")}`)
@@ -104,13 +99,41 @@ function applyHttpSettingsToOperations(operation: MacroApi.Context.IElementApi, 
         httpSettings.getProperty(httpSettingsMediatypeId).setValue("application/json");
     }
 }
-*/
 
+function isMappedDomainOperation(operation: IElementApi) :boolean{
+    var mappings = getMappedRequestDetails(operation);
+    if (mappings == null) return false;
+    return mappings.mappingTargetType === "Operation";
+}
 
-/**
- * Used by Intent.Modules\Modules\Intent.Metadata.WebApi
- *
- * Source code here:
- * https://github.com/IntentArchitect/Intent.Modules/blob/master/DesignerMacros/src/services-expose-as-http-endpoint/service/expose-as-http-endpoint.ts
- */
-exposeAsHttpEndPoint(element);
+function getRouteInfo(operation: IElementApi, routePrefix:string, addId: boolean, entity?: IElementApi, additionalRoute?:string):string {
+
+    let result = routePrefix;
+    if (addId == true){
+        let routeIds:string[] = [];
+
+        if (entity == null){        
+            if (operation.getChildren().some(x => x.getName().toLowerCase() == "id")){
+                routeIds.push(`{id}`);
+            }
+        }else{
+            let primaryKeys = DomainHelper.getPrimaryKeys(entity);            
+            for (const key of primaryKeys) {
+                if (operation.getChildren().some(x => x.getName().toLowerCase() == key.name.toLowerCase())){
+                    routeIds.push(`{${operation.getChildren().find(x => x.getName().toLowerCase() == key.name.toLowerCase()).getName()}}`);
+                }
+            }
+        }
+        if (result.length > 0){
+                result += "/"
+        }
+        result += routeIds.join("/");
+    }
+    if (additionalRoute != null){
+        if (result.length > 0){
+                result += "/"
+        }
+        result += additionalRoute;
+    }
+    return result;
+}
