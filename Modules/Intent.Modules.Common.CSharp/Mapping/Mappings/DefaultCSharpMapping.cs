7b1181b669f6747b9bcc85fffb8c47a114b6e986
Jonathan Lydall <jonathan.lydall@intentarchitect.com>
2025-07-02 15:14:02 +0200

    Guard against a null, but doesn't actually get hit in practice unless you've failed to make a mapping on the "root" on each side, in which case you still get a different (albeit slightly more helpful) exception.

--- Modules/Intent.Modules.Common.CSharp/Mapping/Mappings/DefaultCSharpMapping.cs
+++ Modules/Intent.Modules.Common.CSharp/Mapping/Mappings/DefaultCSharpMapping.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using Intent.Metadata.Models;
 using Intent.Modules.Common.CSharp.Templates;
 
@@ -16,6 +15,6 @@ public static class ElementToElementMappedEndExtensions
 {
     public static bool IsOneToOne(this IElementToElementMappedEnd model)
     {
-        return model.Sources.Count() == 1 && model.MappingExpression.Trim() == $"{{{model.Sources.Single().ExpressionIdentifier}}}";
+        return model?.Sources.Count() == 1 && model.MappingExpression.Trim() == $"{{{model.Sources.Single().ExpressionIdentifier}}}";
     }
 }
