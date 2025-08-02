0f7438aaf082625ef5dc8ac07de6f85f10f29803
Joel Turner <joel.turner@intentarchitect.com>
2025-08-02 15:24:29 +0200

    Decouple MudBlazor

---
+++ Modules/Intent.Modules.Modelers.UI.Core/Api/ComponentModelStereotypeExtensions.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Intent.Metadata.Models;
+using Intent.Modelers.UI.Api;
+using Intent.Modules.Common;
+using Intent.RoslynWeaver.Attributes;
+
+[assembly: DefaultIntentManaged(Mode.Fully)]
+[assembly: IntentTemplate("Intent.ModuleBuilder.Templates.Api.ApiElementModelExtensions", Version = "1.0")]
+
+namespace Intent.Modelers.UI.Core.Api
+{
+    public static class ComponentModelStereotypeExtensions
+    {
+        public static Secured GetSecured(this ComponentModel model)
+        {
+            var stereotype = model.GetStereotype(Secured.DefinitionId);
+            return stereotype != null ? new Secured(stereotype) : null;
+        }
+
+        public static IReadOnlyCollection<Secured> GetSecureds(this ComponentModel model)
+        {
+            var stereotypes = model
+                .GetStereotypes(Secured.DefinitionId)
+                .Select(stereotype => new Secured(stereotype))
+                .ToArray();
+
+            return stereotypes;
+        }
+
+
+        public static bool HasSecured(this ComponentModel model)
+        {
+            return model.HasStereotype(Secured.DefinitionId);
+        }
+
+        public static bool TryGetSecured(this ComponentModel model, out Secured stereotype)
+        {
+            if (!HasSecured(model))
+            {
+                stereotype = null;
+                return false;
+            }
+
+            stereotype = new Secured(model.GetStereotype(Secured.DefinitionId));
+            return true;
+        }
+
+        public class Secured
+        {
+            private IStereotype _stereotype;
+            public const string DefinitionId = "012f5173-6419-4006-a9a8-ab5c20b8a42e";
+
+            public Secured(IStereotype stereotype)
+            {
+                _stereotype = stereotype;
+            }
+
+            public string Name => _stereotype.Name;
+
+            public string Roles()
+            {
+                return _stereotype.GetProperty<string>("Roles");
+            }
+
+            public string Policy()
+            {
+                return _stereotype.GetProperty<string>("Policy");
+            }
+
+        }
+
+    }
+}
--- Modules/Intent.Modules.Modelers.UI.Core/Api/TableModelStereotypeExtensions.cs
+++ Modules/Intent.Modules.Modelers.UI.Core/Api/TableModelStereotypeExtensions.cs
@@ -60,6 +60,40 @@ namespace Intent.Modelers.UI.Core.Api
             return true;
         }
 
+        public static Secured GetSecured(this TableModel model)
+        {
+            var stereotype = model.GetStereotype(Secured.DefinitionId);
+            return stereotype != null ? new Secured(stereotype) : null;
+        }
+
+        public static IReadOnlyCollection<Secured> GetSecureds(this TableModel model)
+        {
+            var stereotypes = model
+                .GetStereotypes(Secured.DefinitionId)
+                .Select(stereotype => new Secured(stereotype))
+                .ToArray();
+
+            return stereotypes;
+        }
+
+
+        public static bool HasSecured(this TableModel model)
+        {
+            return model.HasStereotype(Secured.DefinitionId);
+        }
+
+        public static bool TryGetSecured(this TableModel model, out Secured stereotype)
+        {
+            if (!HasSecured(model))
+            {
+                stereotype = null;
+                return false;
+            }
+
+            stereotype = new Secured(model.GetStereotype(Secured.DefinitionId));
+            return true;
+        }
+
         public class Interaction
         {
             private IStereotype _stereotype;
@@ -104,6 +138,30 @@ namespace Intent.Modelers.UI.Core.Api
             public string OnPageChanged()
             {
                 return _stereotype.GetProperty<string>("On Page Changed");
+            }
+
+        }
+
+        public class Secured
+        {
+            private IStereotype _stereotype;
+            public const string DefinitionId = "012f5173-6419-4006-a9a8-ab5c20b8a42e";
+
+            public Secured(IStereotype stereotype)
+            {
+                _stereotype = stereotype;
+            }
+
+            public string Name => _stereotype.Name;
+
+            public string Roles()
+            {
+                return _stereotype.GetProperty<string>("Roles");
+            }
+
+            public string Policy()
+            {
+                return _stereotype.GetProperty<string>("Policy");
             }
 
         }
