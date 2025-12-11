c2ed88796a5b415d355dc8fb9b42b1f14cb79c25
Jonathan Lydall <jonathan.lydall@intentarchitect.com>
2025-12-11 14:26:41 +0200

    Update names

---
+++ Modules/Intent.Modules.Common.CSharp/CodeToModelSynchronization/CSharpDifferenceType.cs
@@ -0,0 +1,10 @@
+﻿#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+namespace Intent.Modules.Common.CSharp.CodeToModelSynchronization;
+
+public enum CSharpDifferenceType
+{
+    Unchanged = 1,
+    Added = 2,
+    Removed = 3,
+    Changed = 4
+}
