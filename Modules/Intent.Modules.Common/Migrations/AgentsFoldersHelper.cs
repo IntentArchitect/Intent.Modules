d865da96260fc242736324b78fc9cc23767422b7
JP <jp.seini@intentarchitect.com>
2026-04-29 12:12:26 +0200

    Updated max supportedClientVersions Fixed Common migration issue

--- Modules/Intent.Modules.Common/Migrations/AgentsFoldersHelper.cs
+++ Modules/Intent.Modules.Common/Migrations/AgentsFoldersHelper.cs
@@ -11,7 +11,7 @@ namespace Intent.Modules.Common.Migrations
     {
         private const string CodebaseStructureDesignerId = "0701433c-36c0-4569-b1f4-9204986b587d";
         private const string AIContextAnchorId = "5489f14c-82ab-438a-9fef-67604cb25122";
-        private const string AIContextRoleId = "23713fb0-c0ee-4159-bcab-b9d6de1872fc";
+        private const string AIContextRoleId = "23713fb0-c0ee-4159-bcab-b9d6de1872fc";         
         private const string FolderStructureDesignerId = "27916ede-58c6-4356-87b1-0b0628d1e2a7";
 
         internal static void AddAgentsFolder(IApplicationPersistable application)
@@ -30,7 +30,8 @@ namespace Intent.Modules.Common.Migrations
 
             if (package != null && designer.Id == CodebaseStructureDesignerId)
             {
-                if (!package.Classes.TryGet(AIContextAnchorId, out var _))
+                if (!package.Classes.TryGet(AIContextAnchorId, out var _) &&
+                    !package.Classes.Any(c => c.Name == "AI.Context" && c.SpecializationTypeId == PackageExtensions.AnchorSpecicalizationTypeId))
                 {
                     var agentsFolder = package.AddFolder(package.Id, ".agents");
                     var instructionsFolder = package.AddFolder(agentsFolder.Id, "instructions");
@@ -46,7 +47,8 @@ namespace Intent.Modules.Common.Migrations
 
             if (package != null && designer.Id == FolderStructureDesignerId)
             {
-                if (!package.Classes.TryGet(AIContextRoleId, out var _))
+                if (!package.Classes.TryGet(AIContextRoleId, out var _) &&
+                    !package.Classes.Any(c => c.Name == "AI.Context" && c.SpecializationTypeId == PackageExtensions.RoleSpecializationTypeId))
                 {
                     var agentsFolder = package.AddFolder(package.Id, ".agents");
                     var instructionsFolder = package.AddFolder(agentsFolder.Id, "instructions");
@@ -65,7 +67,7 @@ namespace Intent.Modules.Common.Migrations
     internal static class PackageExtensions
     {
         private const string NonVSFolderSpecicalizationTypeId = "4d95d53a-8855-4f35-aa82-e312643f5c5f";
-        private const string AnchorSpecicalizationTypeId = "025e933b-b602-4b6d-95ab-0ec36ae940da";
+        public const string AnchorSpecicalizationTypeId = "025e933b-b602-4b6d-95ab-0ec36ae940da";
         public const string RoleSpecializationTypeId = "8663c9f9-2852-45e7-aaa1-0883a2e6f1da";
 
         internal static IElementPersistable AddAnchor(this IPackageModelPersistable item, string parentId, string name, string anchorId = null)
