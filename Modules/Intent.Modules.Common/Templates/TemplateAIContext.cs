d0e94724476a425812b925eaea076e41ad84f522
Dandré <dandrejvv@users.noreply.github.com>
2026-05-13 13:18:15 +0200

    Revert

--- Modules/Intent.Modules.Common/Templates/TemplateAIContext.cs
+++ Modules/Intent.Modules.Common/Templates/TemplateAIContext.cs
@@ -3,16 +3,16 @@ using Intent.Templates;
 
 namespace Intent.Modules.Common.Templates;
 
-public class TemplateAIContext
+public class TemplateAIContext : IAIContext
 {
     public string Role { get; set; }
     public string UsageGuidelines { get; set; }
     public string Instructions { get; set; }
-    //public IList<IRelatedFile> RelatedFiles { get; set; }
+    public IList<IRelatedFile> RelatedFiles { get; set; }
     public IDictionary<string, object> AdditionalMetadata { get; set; }
 }
 
-public class RelatedFile
+public class RelatedFile : IRelatedFile
 {
     public string FilePath { get; init; }
     public string Reason { get; init; }
