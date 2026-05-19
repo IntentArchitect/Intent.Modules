e5525eef415e149e64b51b3aa39a0f12d36c0c07
JP <jp.seini@intentarchitect.com>
2026-05-19 19:40:34 +0200

    Update TS builder tests

--- Modules/Intent.Modules.Common.TypeScript.Tests/Builder/TypescriptFileTests.cs
+++ Modules/Intent.Modules.Common.TypeScript.Tests/Builder/TypescriptFileTests.cs
@@ -49,5 +49,36 @@ namespace Intent.Modules.Common.TypeScript.Tests.Builder
             builder.CompleteBuild();
             var result = builder.ToString();
         }
+
+        [Fact]
+        public void ImportsAreSortedByBestPractices()
+        {
+            // Arrange
+            var builder = new TypescriptFile(string.Empty, null);
+            builder
+                .AddImport("utils", "./utils")           // Relative import
+                .AddImport("Component", "react")         // External import
+                .AddImport("Config", "@config/settings") // Absolute import
+                .AddImport("helpers", "../helpers")      // Relative import
+                .AddImport("axios", "axios")             // External import
+                .StartBuild()
+                .CompleteBuild();
+
+            // Act
+            var result = builder.ToString();
+
+            // Assert
+            // External imports should come first (axios, react), then absolute imports (@config/settings), then relative imports (../helpers, ./utils)
+            var axiosIndex = result.IndexOf("import");
+            var reactIndex = result.IndexOf("import", axiosIndex + 1);
+            var absoluteIndex = result.IndexOf("import", reactIndex + 1);
+            var relativeUpIndex = result.IndexOf("import", absoluteIndex + 1);
+            var relativeDotIndex = result.IndexOf("import", relativeUpIndex + 1);
+
+            Assert.True(axiosIndex < reactIndex, "axios should come before react");
+            Assert.True(reactIndex < absoluteIndex, "react should come before @config imports");
+            Assert.True(absoluteIndex < relativeUpIndex, "@config should come before relative imports");
+            Assert.True(relativeUpIndex < relativeDotIndex, "../helpers should come before ./utils");
+        }
     }
 }
